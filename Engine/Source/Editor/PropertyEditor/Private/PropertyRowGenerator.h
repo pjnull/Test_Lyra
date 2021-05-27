28bee7c543426bdc32ef08a4f8c93f4fe40e5f2a
lauren barnes <lauren.barnes@epicgames.com>
2021-05-27 14:15:17 -0400

    Edigrating 16251797 to add the custom validation delegate to PropertyRowGenerator, and implementing the custom validation delegate in the Material Layer UI for the Material Instance Editor #rb Matt.Kuhlenschmidt
    #ROBOMERGE-SOURCE: CL 16488754 via CL 16488761 via CL 16488768
    #ROBOMERGE-BOT: (v823-16466674)
    
    [CL 16488772 by lauren barnes in ue5-release-engine-staging branch]

--- Engine/Source/Editor/PropertyEditor/Private/PropertyRowGenerator.h
+++ Engine/Source/Editor/PropertyEditor/Private/PropertyRowGenerator.h
@@ -93,6 +93,7 @@ public:
 	virtual void RegisterInstancedCustomPropertyTypeLayout(FName PropertyTypeName, FOnGetPropertyTypeCustomizationInstance PropertyTypeLayoutDelegate, TSharedPtr<IPropertyTypeIdentifier> Identifier = nullptr) override;
 	virtual void UnregisterInstancedCustomPropertyLayout(UStruct* Class) override;
 	virtual void UnregisterInstancedCustomPropertyTypeLayout(FName PropertyTypeName, TSharedPtr<IPropertyTypeIdentifier> Identifier = nullptr) override;
+	}
 	virtual void SetCustomValidatePropertyNodesFunction(FOnValidatePropertyRowGeneratorNodes InCustomValidatePropertyNodesFunction) override
 	{
 		CustomValidatePropertyNodesFunction = MoveTemp(InCustomValidatePropertyNodesFunction);
