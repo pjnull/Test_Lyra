9341310eec86e058a5d21b73b8b4b1b3367e523b
Patrick Boutot <Patrick.Boutot@epicgames.com>
2021-05-29 20:43:32 -0400

    Fix CIS error message for LevelSnapshotEditor. #rb none #rnx
    [CL 16505820 by Patrick Boutot in ue5-release-engine-staging branch]

--- Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshotsEditor/Private/Views/Results/SLevelSnapshotsEditorResults.cpp
+++ Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshotsEditor/Private/Views/Results/SLevelSnapshotsEditorResults.cpp
@@ -2721,17 +2721,11 @@ void SLevelSnapshotsEditorResultsRow::Construct(const FArguments& InArgs, const 
 		.HitDetectionSplitterHandleSize(bDoesRowNeedSplitter ? 5.0f : 0.f);
 
 		SplitterPtr->AddSlot()
+		.OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetNameColumnSize)
+		.Value(this, &SLevelSnapshotsEditorResultsRow::GetNameColumnSize)
 		[
 			BasicRowWidgets
 		];
-		
-		// Splitter Slot 0
-		int32 SlotIndex = 0;
-		{
-			SplitterPtr->SlotAt(SlotIndex).OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetNameColumnSize);
-			const auto SlotDelegate = TAttribute<float>::FGetter::CreateSP(this, &SLevelSnapshotsEditorResultsRow::GetNameColumnSize);;
-			SplitterPtr->SlotAt(SlotIndex).SizeValue.Bind(SlotDelegate);
-		}
 	
 		// Splitter Slot 1
 		SlotIndex = 1;
@@ -2811,15 +2805,11 @@ void SLevelSnapshotsEditorResultsRow::Construct(const FArguments& InArgs, const 
 		}
 
 		SplitterPtr->AddSlot()
+		.OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetWorldColumnSize)
+		.Value(this, &SLevelSnapshotsEditorResultsRow::GetWorldColumnSize)
 		[
 			FinalWorldWidget.ToSharedRef()
 		];
-
-		{
-			SplitterPtr->SlotAt(SlotIndex).OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetWorldColumnSize);
-			const auto SlotDelegate = TAttribute<float>::FGetter::CreateSP(this, &SLevelSnapshotsEditorResultsRow::GetWorldColumnSize);
-			SplitterPtr->SlotAt(SlotIndex).SizeValue.Bind(SlotDelegate);
-		}
 
 		// Splitter Slot 2
 		SlotIndex = 2;
@@ -2899,15 +2889,11 @@ void SLevelSnapshotsEditorResultsRow::Construct(const FArguments& InArgs, const 
 		}
 
 		SplitterPtr->AddSlot()
+		.OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetSnapshotColumnSize)
+		.Value(this, &SLevelSnapshotsEditorResultsRow::GetSnapshotColumnSize)
 		[
 			FinalSnapshotWidget.ToSharedRef()
 		];
-
-		{
-			SplitterPtr->SlotAt(SlotIndex).OnSlotResized(this, &SLevelSnapshotsEditorResultsRow::SetSnapshotColumnSize);
-			const auto SlotDelegate = TAttribute<float>::FGetter::CreateSP(this, &SLevelSnapshotsEditorResultsRow::GetSnapshotColumnSize);
-			SplitterPtr->SlotAt(SlotIndex).SizeValue.Bind(SlotDelegate);
-		}
 		
 		BorderPtr->SetContent(SplitterPtr.ToSharedRef());
 	}
