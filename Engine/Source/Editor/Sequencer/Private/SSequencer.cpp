51811d9945a57ee21027ed9add0c5867eadc86cc
max chen <Max.Chen@epicgames.com>
2021-06-07 13:58:08 -0400

    Sequencer: Added a delegate OnCameraAddedToSequencer which can be implemented to respond to when a camera is added to Sequencer. The return value, when true will execute the default behavior of locking the camera to the viewport and adding a camera cut.
    #jira UE-105319
    #rb mike.zyracki, ludovic.chabant
    #lockdown cristina.riveron
    
    
    #ROBOMERGE-OWNER: max.chen
    #ROBOMERGE-AUTHOR: max.chen
    #ROBOMERGE-SOURCE: CL 16571175 via CL 16571186 via CL 16571213
    #ROBOMERGE-BOT: (v828-16531559)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16574223 by max chen in ue5-release-engine-staging branch]

--- Engine/Source/Editor/Sequencer/Private/SSequencer.cpp
+++ Engine/Source/Editor/Sequencer/Private/SSequencer.cpp
@@ -3322,9 +3322,12 @@ void SSequencer::OnAssetsDropped( const FAssetDragDropOp& DragDropOp )
 				{
 					FMovieSceneSpawnable* Spawnable = MovieScene->FindSpawnable(NewGuid);
 
-					if (Spawnable && Spawnable->GetObjectTemplate()->IsA<ACameraActor>())
+					if (Spawnable)
 					{
-						SequencerRef.NewCameraAdded(NewGuid);
+						if (ACameraActor* CameraActor = Cast<ACameraActor>(Spawnable->GetObjectTemplate()))
+						{
+							SequencerRef.NewCameraAdded(CameraActor, NewGuid);
+						}
 					}
 				}
 			}
