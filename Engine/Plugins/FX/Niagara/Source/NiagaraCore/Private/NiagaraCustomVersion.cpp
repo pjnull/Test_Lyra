c35599a8fe3e953283f2f21927039749469224d6
michael forot <Michael.Forot@epicgames.com>
2021-06-09 09:34:14 -0400

    Add pushout option the the hair collisions
    #rb charles.derousiers
    #jira MH-3721
    #lockdown cristina.riveron
    
    #ROBOMERGE-OWNER: michael.forot
    #ROBOMERGE-AUTHOR: michael.forot
    #ROBOMERGE-SOURCE: CL 16584200 in //UE4/Release-4.27/... via CL 16584204
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-Engine-Staging) (v828-16531559)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16604106 by michael forot in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x03BD6918, 0xE41E8A4D, 0xB8A1BA26, 0xD7E638E4);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x47D28D97, 0x8B994912, 0x9C3A687F, 0x8B6EFCBA);
+
