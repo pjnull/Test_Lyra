25afad08989b0fcb069f0a14a4bd53c74f554687
andrew grant <Andrew.Grant@epicgames.com>
2021-06-11 17:25:11 -0400

    Added config file utility in Gauntlet that allows custom tests to suppress known warnings/ensures that may have originated from mainline.
    (Not used in any default tests, games should override CreateLogSummaryFromArtifact in their test nodes and use the config file there to suppress issues if so desired)
    
    [at]eric.knapik
    #preflight 60c3613ac612640001f1c613
    
    
    #ROBOMERGE-OWNER: andrew.grant
    #ROBOMERGE-AUTHOR: andrew.grant
    #ROBOMERGE-SOURCE: CL 16645136 via CL 16646817 via CL 16646827 via CL 16646854
    #ROBOMERGE-BOT: (v833-16641396)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16647523 by andrew grant in ue5-release-engine-staging branch]

---
+++ Engine/Source/Programs/AutomationTool/Gauntlet/Framework/Utils/Gauntlet.IgnoredIssueList.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauntlet
+{
+	/// <summary>
+	/// Information that defines a device
+	/// </summary>
+	class IgnoredTestIssues
+	{
+		public string TestName = "";
+
+		public string[] IgnoredEnsures = new string[0];
+
+		public string[] IgnoredWarnings = new string[0];
+
+		public string[] IgnoredErrors = new string[0];
+	}
+		
+	/// <summary>
+	/// A class that can be serialized in from JSON and used in a TestNode to ignore specific warnings/errors if so desired
+	/// </summary>
+	public class IgnoredIssueList
+	{
+		public IgnoredIssueList()
+		{
+
+		}
+
+		private IEnumerable<IgnoredTestIssues> IgnoredIssues { get; set; } = Enumerable.Empty<IgnoredTestIssues>();
+
+		public bool LoadFromFile(string InPath)
+		{
+			try
+			{
+				IgnoredIssues = JsonConvert.DeserializeObject<IgnoredTestIssues[]>(File.ReadAllText(InPath));
+			}
+			catch (Exception Ex)
+			{
+				Log.Warning("Failed to load issue definition from {0}. {1}", InPath, Ex.Message);
+				return false;
+			}
+
+			return true;
+		}
+
+		public bool IsEnsureIgnored(string InTestName, string InEnsureMsg)
+		{
+			IEnumerable<IgnoredTestIssues> TestDefinitions = IgnoredIssues.Where(I => I.TestName == "*" || I.TestName.Equals(InTestName, StringComparison.OrdinalIgnoreCase));
+			return TestDefinitions.Any(D => D.IgnoredEnsures.Any(Ex => InEnsureMsg.IndexOf(Ex, StringComparison.OrdinalIgnoreCase) >= 0));
+
+		}
+
+		public bool IsWarningIgnored(string InTestName, string InWarning)
+		{
+			IEnumerable<IgnoredTestIssues> TestDefinitions = IgnoredIssues.Where(I => I.TestName == "*" || I.TestName.Equals(InTestName, StringComparison.OrdinalIgnoreCase));
+			return TestDefinitions.Any(D => D.IgnoredWarnings.Any( Ex => InWarning.IndexOf(Ex, StringComparison.OrdinalIgnoreCase) >= 0));
+
+		}
+
+		public bool IsErrorIgnored(string InTestName, string InError)
+		{
+			IEnumerable<IgnoredTestIssues> TestDefinitions = IgnoredIssues.Where(I => I.TestName.Equals(InTestName, StringComparison.OrdinalIgnoreCase));
+			return TestDefinitions.Any(D => D.IgnoredErrors.Any(Ex => InError.IndexOf(Ex, StringComparison.OrdinalIgnoreCase) >= 0));
+		}
+	}
+}
