21e2e95d0018ae92e42093980ce8a8fa925aff7d
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-06-14 17:53:51 -0400

    Niagara VM byte code compilation fix for self assignment caused by attribute masking and not clearing the replacement history when the variable is reassigned: A = A + B; // should be replaced by: x = A + B C = A + D; // should be replaced by: C = x + D; A = E; // should be fine as is F = A + G; // should be fine as is, we can't have A being replaced by x here
    #rb simon.tovey
    #jira UE-117917
    
    
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 16665166 via CL 16665188 via CL 16665209
    #ROBOMERGE-BOT: (v834-16658389)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16665252 by rob krajcarski in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x322B8494, 0xA98E469F, 0x8EB5C628, 0xF0DDC089);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x97A81196, 0xE2E84107, 0x8973C589, 0x78744FEB);
