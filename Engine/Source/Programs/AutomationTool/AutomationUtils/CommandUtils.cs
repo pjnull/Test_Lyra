5298f49697ef2f9c4354a4cf83c0a08b1a2a6732
ryan durand <Ryan.Durand@epicgames.com>
2021-06-17 16:13:03 -0400

    Make sure to break after a successful copy.
    #rnx
    #rb none
    #jira none
    
    #ROBOMERGE-OWNER: ryan.durand
    #ROBOMERGE-AUTHOR: ryan.durand
    #ROBOMERGE-SOURCE: CL 16710154 in //UE5/Main/... via CL 16710163
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v835-16672529)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16710366 by ryan durand in ue5-release-engine-staging branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/CommandUtils.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/CommandUtils.cs
@@ -1329,11 +1329,7 @@ namespace AutomationTool
 					}
 					throw new AutomationException("Failed to copy {0} to {1}", Source, Dest);
 				}
-				else
-				{
-					// success 
-					break;
-				}
+				break;
 			}
 		}
 
