51b03ea9cb4f4d631d9a00583bab6beffe30c01f
bob tellez <Bob.Tellez@epicgames.com>
2021-06-24 17:51:18 -0400

    Back out changelist 16773089 and 16776837. Starving thread pool
    [FYI] Michael.Galetzka
    
    
    #ROBOMERGE-OWNER: bob.tellez
    #ROBOMERGE-AUTHOR: bob.tellez
    #ROBOMERGE-SOURCE: CL 16778975 via CL 16778983 via CL 16778990
    #ROBOMERGE-BOT: (v836-16769935)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 16779137 by bob tellez in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xA83D40AB, 0xE1F64D9F, 0x9F5451FB, 0x8C954559);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x9D34C1D4, 0x51864B89, 0x8AB8F373, 0x477A110E);
