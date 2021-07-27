5ad732176863a7b6e02344ed9d3ca9059731da57
sara schvartzman <sara.schvartzman@epicgames.com>
2021-07-27 09:59:25 -0400

    Control Rig: Python commands output to different message log pages depending on the blueprint #jira UE-119792 #rb helge.mathee
    #ROBOMERGE-SOURCE: CL 16968244 in //UE5/Main/... via CL 16968245
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v838-16927207)
    
    [CL 16968251 by sara schvartzman in ue5-release-engine-staging branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprint.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprint.cpp
@@ -3516,7 +3516,8 @@ void UControlRigBlueprint::OnVariableAdded(const FName& InVarName)
 
 	FRigVMExternalVariable ExternalVariable = UControlRig::GetExternalVariableFromDescription(Variable);
 
-	RigVMPythonUtils::Print(FString::Printf(TEXT("blueprint.add_member_variable('%s', '%s', %s, %s, '%s')"),
+	RigVMPythonUtils::Print(GetFName().ToString(),
+		FString::Printf(TEXT("blueprint.add_member_variable('%s', '%s', %s, %s, '%s')"),
 			*InVarName.ToString(),
 			*ExternalVariable.TypeName.ToString(),
 			(ExternalVariable.bIsPublic) ? TEXT("False") : TEXT("True"), 
@@ -3537,7 +3538,8 @@ void UControlRigBlueprint::OnVariableRemoved(const FName& InVarName)
 		}
 	}
 
-	RigVMPythonUtils::Print(FString::Printf(TEXT("blueprint.remove_member_variable('%s')"),
+	RigVMPythonUtils::Print(GetFName().ToString(),
+		FString::Printf(TEXT("blueprint.remove_member_variable('%s')"),
 			*InVarName.ToString()));
 	
 	BroadcastExternalVariablesChangedEvent();
@@ -3554,7 +3556,8 @@ void UControlRigBlueprint::OnVariableRenamed(const FName& InOldVarName, const FN
 		}
 	}
 
-	RigVMPythonUtils::Print(FString::Printf(TEXT("blueprint.rename_member_variable('%s', '%s')"),
+	RigVMPythonUtils::Print(GetFName().ToString(),
+		FString::Printf(TEXT("blueprint.rename_member_variable('%s', '%s')"),
 			*InOldVarName.ToString(),
 			*InNewVarName.ToString()));
 	
@@ -3609,7 +3612,8 @@ void UControlRigBlueprint::OnVariableTypeChanged(const FName& InVarName, FEdGrap
 		}
 	}
 
-	RigVMPythonUtils::Print(FString::Printf(TEXT("blueprint.change_member_variable_type('%s', '%s')"),
+	RigVMPythonUtils::Print(GetFName().ToString(),
+		FString::Printf(TEXT("blueprint.change_member_variable_type('%s', '%s')"),
 		*InVarName.ToString(),
 		*PinType));
 
