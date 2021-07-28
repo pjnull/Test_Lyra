a14a4e3f08a9f31c83a9c659a6a6953ef75a468e
christopher waters <christopher.waters@epicgames.com>
2021-07-28 17:20:02 -0400

    EBufferUsageFlags as enum class.
    #rb will.damon, rolando.caloca
    #preflight 610179041e63730001feb1eb
    #preflight 6101b0d5b4288d0001b61c32
    
    #ROBOMERGE-OWNER: christopher.waters
    #ROBOMERGE-AUTHOR: christopher.waters
    #ROBOMERGE-SOURCE: CL 16986666 via CL 16987824 via CL 16987849
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v838-16927207)
    
    [CL 16987861 by christopher waters in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/D3D12RHI/Private/D3D12Adapter.h
+++ Engine/Source/Runtime/D3D12RHI/Private/D3D12Adapter.h
@@ -343,7 +343,7 @@ public:
 
 	FD3D12Buffer* CreateRHIBuffer(
 		const D3D12_RESOURCE_DESC& Desc,
-		uint32 Alignment, uint32 Stride, uint32 Size, uint32 InUsage,
+		uint32 Alignment, uint32 Stride, uint32 Size, EBufferUsageFlags InUsage,
 		ED3D12ResourceStateMode InResourceStateMode,
 		D3D12_RESOURCE_STATES InCreateState,
 		bool bHasInitialData,
@@ -429,7 +429,7 @@ protected:
 	virtual void AllocateBuffer(FD3D12Device* Device,
 		const D3D12_RESOURCE_DESC& Desc,
 		uint32 Size,
-		uint32 InUsage,
+		EBufferUsageFlags InUsage,
 		ED3D12ResourceStateMode InResourceStateMode,
 		D3D12_RESOURCE_STATES InCreateState,
 		uint32 Alignment,
