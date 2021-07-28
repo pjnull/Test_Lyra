bfddbd0badaefa00eafbb5142e43c24c0cd43012
uriel doyon <Uriel.Doyon@epicgames.com>
2021-07-27 22:49:43 -0400

    Removed error spam when loading cooked texture in the editor. This would happen when the texture mips are loaded in sync. #preflight 610059ef390eb40001210d28 #rb alexis.matte
    #ROBOMERGE-SOURCE: CL 16972879 via CL 16974498 via CL 16978285 via CL 16978355
    #ROBOMERGE-BOT: (v838-16927207)
    
    [CL 16978379 by uriel doyon in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
+++ Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
@@ -1081,7 +1081,8 @@ bool FTexturePlatformData::TryLoadMips(int32 FirstMipToLoad, void** OutMipData, 
 				// We want to make sure that any non-streamed mips are coming from the texture asset file, and not from an external bulk file.
 				// But because "r.TextureStreaming" is driven by the project setting as well as the command line option "-NoTextureStreaming", 
 				// is it possible for streaming mips to be loaded in non streaming ways.
-				if (CVarSetTextureStreaming.GetValueOnAnyThread() != 0)
+				// Also check if editor data is available, in which case we are probably loading cooked data in the editor.
+				if (!FPlatformProperties::HasEditorOnlyData() && CVarSetTextureStreaming.GetValueOnAnyThread() != 0)
 				{
 					UE_CLOG(Mip.BulkData.IsInSeparateFile(), LogTexture, Error, TEXT("Loading non-streamed mips from an external bulk file.  This is not desireable.  File %s"),
 						*(Mip.BulkData.GetPackagePath().GetDebugName()) );
