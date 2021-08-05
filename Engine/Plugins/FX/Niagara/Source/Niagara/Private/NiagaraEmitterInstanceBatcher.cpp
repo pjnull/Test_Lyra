5c69d1d5d1032d50594cd5ed5ce65b990d743c57
ben woodhouse <Ben.Woodhouse@epicgames.com>
2021-08-04 20:55:31 -0400

    Niagara compile fix [FYI] stu.mckenna #rb simon.tovey
    #ROBOMERGE-SOURCE: CL 17049322 via CL 17057693 via CL 17058512
    #ROBOMERGE-BOT: (v850-17047176)
    
    [CL 17062667 by ben woodhouse in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraEmitterInstanceBatcher.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraEmitterInstanceBatcher.cpp
@@ -507,6 +507,7 @@ void NiagaraEmitterInstanceBatcher::ProcessPendingTicksFlush(FRHICommandListImme
 			CreateSystemTextures(GraphBuilder);
 			PreInitViews(GraphBuilder, AllowGPUParticleUpdate);
 			GPUInstanceCounterManager.UpdateDrawIndirectBuffers(*this, RHICmdList, FeatureLevel, ENiagaraGPUCountUpdatePhase::PreOpaque);
+			GPUInstanceCounterManager.UpdateDrawIndirectBuffers(*this, RHICmdList, FeatureLevel, ENiagaraGPUCountUpdatePhase::PostOpaque);
 			PostInitViews(GraphBuilder, DummyViews, AllowGPUParticleUpdate);
 			PostRenderOpaque(GraphBuilder, DummyViews, AllowGPUParticleUpdate);
 			GPUInstanceCounterManager.UpdateDrawIndirectBuffers(*this, RHICmdList, FeatureLevel, ENiagaraGPUCountUpdatePhase::PostOpaque);
