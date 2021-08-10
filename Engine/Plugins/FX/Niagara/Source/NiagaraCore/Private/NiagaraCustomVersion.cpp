1ea4ff0828d330848a5f022273ea312bc3590ed3
stu mckenna <Stu.McKenna@epicgames.com>
2021-08-10 12:29:54 -0400

    - All GPU stages are now 'simulation stages' - The intrinsic particle stage is now just another simulation stage that can be enabled or not - Iteration count is now dynamic and can be bound to a user variable.  In the future we should be able to skip compilation on the constant version. - Stages can be enabled / disabled dynamically by a user variable - Brought some clarification to SpawnOnly on simulation stages
    #rb shaun.kime, devon.penney
    #preflight 6111aefa0ccb84000194d07f
    
    
    
    #ROBOMERGE-OWNER: stu.mckenna
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 17117715 via CL 17117851 via CL 17118222 via CL 17118346
    #ROBOMERGE-BOT: (v855-17104924)
    
    [CL 17118352 by stu mckenna in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xB1DA9F96, 0x61DE9F4C, 0x8574B0FC, 0x0FA14BB3);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x6B3E6834, 0x5C867C44, 0xA090A21A, 0x4D21C080);
 
