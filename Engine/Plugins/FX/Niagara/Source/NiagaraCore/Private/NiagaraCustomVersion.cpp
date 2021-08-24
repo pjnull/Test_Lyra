af66adef643e1a73cdbe6b854efbb9dccf0703a7
michael galetzka <michael.galetzka@epicgames.com>
2021-08-24 16:22:04 -0400

    Fixed a bug where some engine constants could be marked as "read before being set"
    #rb nicholas.goldstein
    
    
    
    #ROBOMERGE-OWNER: michael.galetzka
    #ROBOMERGE-AUTHOR: michael.galetzka
    #ROBOMERGE-SOURCE: CL 17292540 via CL 17292587 via CL 17292779 via CL 17292809
    #ROBOMERGE-BOT: (v861-17282326)
    
    [CL 17292818 by michael galetzka in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xEAFC7ED9, 0x1A2DC946, 0x87082917, 0x4B6B9A7D);
-
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x02497746, 0x33D75C49, 0x9A60CB65, 0x7D6AC5A4);
