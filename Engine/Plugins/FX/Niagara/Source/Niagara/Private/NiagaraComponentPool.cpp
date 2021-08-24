059e442ed023caedcb1623e4bcabf8a3dff14c95
robert manuszewski <Robert.Manuszewski@epicgames.com>
2021-08-23 21:16:18 -0400

    More static analysis fixes
    #rb none
    
    
    #ROBOMERGE-SOURCE: CL 17265971 via CL 17280037 via CL 17280092
    #ROBOMERGE-BOT: (v858-17259218)
    
    [CL 17280096 by robert manuszewski in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraComponentPool.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraComponentPool.cpp
@@ -91,7 +91,7 @@ UNiagaraComponent* FNCPool::Acquire(UWorld* World, UNiagaraSystem* Template, ENC
 	while (FreeElements.Num() && !bForceNew)//Loop until we pop a valid free element or we're empty.
 	{
 		RetElem = FreeElements.Pop(false);
-		if (!IsValid(RetElem.Component))
+		if (!RetElem.Component || !IsValidChecked(RetElem.Component))
 		{			
 			// Possible someone still has a reference to our NC and destroyed it while it was sat in the pool. Or possibly a teardown edgecase path that is GCing components from the pool be
 			UE_LOG(LogNiagara, Warning, TEXT("Pooled NC has been destroyed or is pending kill! Possibly via a DestroyComponent() call. You should not destroy pooled components manually. \nJust deactivate them and allow them to destroy themselves or be reclaimed by the pool. | NC: %p |\t System: %s"), RetElem.Component, *Template->GetFullName());
