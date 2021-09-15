2ebfd3369cf6e38dc174a90228749b966993c541
Mateo Egey <mateo.egey@xa.epicgames.com>
2021-09-15 05:25:40 -0400

    Manual Merging using //UE5/Release-Engine-Test_To_//UE5/Release-Engine-Staging
    Niagara node localization fix for pin names in staticswitch & select node for enums
    Also removed a function that wasn't being used
    
    #jira UE-126582
    #rb rob.krajcarski
    
    [CL 17517243 by Mateo Egey in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,5 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x93423B20, 0x4DB047F2, 0x8EE8FFA4, 0xA309EDAD);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x375F6E63, 0x7F2144F2, 0xAD4EC387, 0x0AB6472D);
 
