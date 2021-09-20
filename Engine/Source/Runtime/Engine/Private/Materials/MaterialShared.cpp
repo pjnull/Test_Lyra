06b88430e6e2571558c0804b73e186dcc3cb1182
arciel rekman <arciel.rekman@epicgames.com>
2021-09-20 19:14:07 -0400

    Fix crashes during the shader compilation when showing NavMesh (UE-117842).
    #rb Ben.Ingram
    [CODEREVIEW] [at]Ben.Ingram
    #jira UE-117842
    
    #ROBOMERGE-AUTHOR: arciel.rekman
    #ROBOMERGE-SOURCE: CL 17577303 in //UE5/Main/... via CL 17577310
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v871-17566257)
    
    [CL 17577311 by arciel rekman in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
@@ -3304,10 +3304,20 @@ FMaterialRenderProxy::FMaterialRenderProxy()
 	, DeletedFlag(0)
 	, HasVirtualTextureCallbacks(0)
 {
+	// MaterialRenderProxyMap is only used by shader compiling
+	if (!FPlatformProperties::RequiresCookedData())
+	{
+		FMaterialRenderProxy::MaterialRenderProxyMap.Add(this);
+	}
 }
 
 FMaterialRenderProxy::~FMaterialRenderProxy()
 {
+	if (!FPlatformProperties::RequiresCookedData())
+	{
+		FMaterialRenderProxy::MaterialRenderProxyMap.Remove(this);
+	}
+
 	if(IsInitialized())
 	{
 		check(IsInRenderingThread());
@@ -3324,22 +3334,8 @@ FMaterialRenderProxy::~FMaterialRenderProxy()
 	DeletedFlag = 1;
 }
 
-void FMaterialRenderProxy::InitDynamicRHI()
-{
-	// MaterialRenderProxyMap is only used by shader compiling
-	if (!FPlatformProperties::RequiresCookedData())
-	{
-		FMaterialRenderProxy::MaterialRenderProxyMap.Add(this);
-	}
-}
-
 void FMaterialRenderProxy::ReleaseDynamicRHI()
 {
-	if (!FPlatformProperties::RequiresCookedData())
-	{
-		FMaterialRenderProxy::MaterialRenderProxyMap.Remove(this);
-	}
-
 	DeferredUniformExpressionCacheRequests.Remove(this);
 
 	InvalidateUniformExpressionCache(true);
