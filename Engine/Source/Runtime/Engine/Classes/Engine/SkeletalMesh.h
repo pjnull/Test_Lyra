37474f7e0f4b01c61705a46fe7bf0d7855d6d1a6
josie yang <Josie.Yang@epicgames.com>
2021-09-27 14:11:58 -0400

    Extend FFinishBuildMorphTargetData class to support async serialization of externally derived UMorphTarget classes
    #rb Alexis.Matte
    #jira UE-118919
    #preflight 614c541f787d1b0001ec6029
    
    #ROBOMERGE-AUTHOR: josie.yang
    #ROBOMERGE-SOURCE: CL 17633454 in //UE5/Main/... via CL 17633459
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v871-17566257)
    
    [CL 17633465 by josie yang in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Classes/Engine/SkeletalMesh.h
+++ Engine/Source/Runtime/Engine/Classes/Engine/SkeletalMesh.h
@@ -122,18 +122,16 @@ enum class ESkeletalMeshAsyncProperties : uint64
 
 ENUM_CLASS_FLAGS(ESkeletalMeshAsyncProperties);
 
+#if WITH_EDITOR
 struct FFinishBuildInternalData
 {
 public:
-	/** Editor MorphTargets data to set. */
-	bool bApplyMorphTargetsData = false;
-	TMap<FName, TArray<FMorphTargetLODModel> > MorphLODModelsPerTargetName;
-
 	void ApplyEditorData(USkeletalMesh* SkeletalMesh) const;
 
-private:
-	void ApplyMorphTargetsEditorData(USkeletalMesh* SkeletalMesh) const;
+	/** Editor MorphTargets data to set. */
+	TUniquePtr<FFinishBuildMorphTargetData> MorphTargetData;
 };
+#endif
 
 class FSkeletalMeshCompilationContext
 {
@@ -145,7 +143,9 @@ public:
 	// Movable
 	FSkeletalMeshCompilationContext(FSkeletalMeshCompilationContext&&) = default;
 	FSkeletalMeshCompilationContext& operator=(FSkeletalMeshCompilationContext&&) = default;
+#if WITH_EDITOR
 	FFinishBuildInternalData FinishBuildInternalData;
+#endif
 };
 
 class FSkeletalMeshPostLoadContext : public FSkeletalMeshCompilationContext
