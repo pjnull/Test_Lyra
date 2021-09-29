7eec87089f36d287bcda01f7ae0e579e4cfa2041
brandon grable <Brandon.Grable@epicgames.com>
2021-09-29 12:24:33 -0400

    Back out changelist 17662088 due to CIS errors [FYI] Josh.Adams
    #ROBOMERGE-AUTHOR: brandon.grable
    #ROBOMERGE-SOURCE: CL 17663143 via CL 17663149 via CL 17663158
    #ROBOMERGE-BOT: (v875-17642767)
    
    [CL 17663160 by brandon grable in 5.0 branch]

--- Engine/Source/Developer/CookedEditor/Public/CookedEditorTargetPlatform.h
+++ Engine/Source/Developer/CookedEditor/Public/CookedEditorTargetPlatform.h
@@ -43,11 +43,6 @@ public:
 		return CachedPlatformName;
 	}
 
-	virtual FString CookingDeviceProfileName() const override
-	{
-		return Base::PlatformName();
-	}
-
 	/**
 	 * If you override this to return false, you will have to stage uncooked assets to allow the editor to run properly
 	 */
@@ -71,7 +66,7 @@ public:
 	{
 		if (AllowsEditorObjects())
 		{
-			PackageManager->GatherAllPackages(PackageNames, this);
+			PackageManager->GatherAllPackages(PackageNames);
 		}
 	}
 
