b043ebe4d7a3654baa81cfe113b754185067a185
ben hoffman <ben.hoffman@epicgames.com>
2021-09-30 14:10:32 -0400

    Fix a crash in Enhanced Input nodes when assets failed to be loaded when opening a blueprint that referenced input actions that may have been saved with an incorrect engine version
    #jira UE-129494
    #rb marc.audy
    #rnx
    
    [CL 17680972 by ben hoffman in 5.0 branch]

--- Engine/Plugins/Experimental/EnhancedInput/Source/InputBlueprintNodes/Private/K2Node_EnhancedInputAction.cpp
+++ Engine/Plugins/Experimental/EnhancedInput/Source/InputBlueprintNodes/Private/K2Node_EnhancedInputAction.cpp
@@ -305,7 +305,7 @@ void UK2Node_EnhancedInputAction::GetMenuActions(FBlueprintActionDatabaseRegistr
 	// Do a first time registration using the node's class to pull in all existing actions
 	if (ActionRegistrar.IsOpenForRegistration(GetClass()))
 	{
-		IAssetRegistry& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry").Get();
+		IAssetRegistry& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry")).Get();
 
 		static bool bRegisterOnce = true;
 		if (bRegisterOnce)
@@ -324,9 +324,11 @@ void UK2Node_EnhancedInputAction::GetMenuActions(FBlueprintActionDatabaseRegistr
 			UBlueprintNodeSpawner* NodeSpawner = UBlueprintNodeSpawner::Create(GetClass());
 			check(NodeSpawner != nullptr);
 
-			const UInputAction* Action = Cast<const UInputAction>(ActionAsset.GetAsset());
-			NodeSpawner->CustomizeNodeDelegate = UBlueprintNodeSpawner::FCustomizeNodeDelegate::CreateStatic(CustomizeInputNodeLambda, TWeakObjectPtr<const UInputAction>(Action));
-			ActionRegistrar.AddBlueprintAction(Action, NodeSpawner);
+			if (const UInputAction* Action = Cast<const UInputAction>(ActionAsset.GetAsset()))
+			{
+				NodeSpawner->CustomizeNodeDelegate = UBlueprintNodeSpawner::FCustomizeNodeDelegate::CreateStatic(CustomizeInputNodeLambda, TWeakObjectPtr<const UInputAction>(Action));
+				ActionRegistrar.AddBlueprintAction(Action, NodeSpawner);	
+			}
 		}
 	}
 	else if (const UInputAction* Action = Cast<const UInputAction>(ActionRegistrar.GetActionKeyFilter()))
