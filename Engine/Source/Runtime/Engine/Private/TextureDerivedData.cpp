945ade5af7c147a977f6f0ced718ac131e919c7f
zousar shaker <zousar.shaker@epicgames.com>
2021-09-30 10:57:13 -0400

    -Remove serialized mip size header from DDC stored mips. -Remove bCooked field from serialized mip format. -Bump texture key version.
    #rb devin.doucette
    
    
    #ROBOMERGE-AUTHOR: zousar.shaker
    #ROBOMERGE-SOURCE: CL 17678129 via CL 17678139 via CL 17678143
    #ROBOMERGE-BOT: (v875-17642767)
    
    [CL 17678144 by zousar shaker in 5.0 branch]

--- Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
+++ Engine/Source/Runtime/Engine/Private/TextureDerivedData.cpp
@@ -57,7 +57,7 @@
 // In case of merge conflicts with DDC versions, you MUST generate a new GUID and set this new
 // guid as version
 
-#define TEXTURE_DERIVEDDATA_VER		TEXT("6F4DB6EC89FB4C34B2415FC04F995708")
+#define TEXTURE_DERIVEDDATA_VER		TEXT("596BF8F951D64FD7A48E0C99F80E2F36")
 
 // This GUID is mixed into DDC version for virtual textures only, this allows updating DDC version for VT without invalidating DDC for all textures
 // This is useful during development, but once large numbers of VT are present in shipped content, it will have the same problem as TEXTURE_DERIVEDDATA_VER
@@ -1096,13 +1096,11 @@ bool FTexturePlatformData::TryInlineMipData(int32 FirstMipToLoad, UTexture* Text
 			}
 			if (bLoadedFromDDC)
 			{
-				int32 MipSize = 0;
 				FMemoryReader Ar(TempData, /*bIsPersistent=*/ true);
-				Ar << MipSize;
 
 				Mip.BulkData.Lock(LOCK_READ_WRITE);
-				void* MipData = Mip.BulkData.Realloc(MipSize);
-				Ar.Serialize(MipData, MipSize);
+				void* MipData = Mip.BulkData.Realloc(TempData.Num());
+				Ar.Serialize(MipData, TempData.Num());
 				Mip.BulkData.Unlock();
 				Mip.SetPagedToDerivedData(false);
 			}
@@ -1280,16 +1278,14 @@ bool FTexturePlatformData::TryLoadMips(int32 FirstMipToLoad, void** OutMipData, 
 				DDC.WaitAsynchronousCompletion(AsyncHandle);
 				if (DDC.GetAsynchronousResults(AsyncHandle, TempData))
 				{
-					int32 MipSize = 0;
 					FMemoryReader Ar(TempData, /*bIsPersistent=*/ true);
-					Ar << MipSize;
-					CheckMipSize(Mip, PixelFormat, MipSize);
+					CheckMipSize(Mip, PixelFormat, TempData.Num());
 					NumMipsCached++;
 
 					if (OutMipData)
 					{
-						OutMipData[MipIndex - FirstMipToLoad] = FMemory::Malloc(MipSize);
-						Ar.Serialize(OutMipData[MipIndex - FirstMipToLoad], MipSize);
+						OutMipData[MipIndex - FirstMipToLoad] = FMemory::Malloc(TempData.Num());
+						Ar.Serialize(OutMipData[MipIndex - FirstMipToLoad], TempData.Num());
 					}
 				}
 				else
