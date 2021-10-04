849b83a58676e963aa5f0250c3ea4ed27ebea7c4
Rob Gay <Rob.Gay@epicgames.com>
2021-10-04 15:47:47 -0400

    Edigrate Fix for SFC #rb trivial #rnx #preflight skip
    [CL 17712687 by Rob Gay in 5.0 branch]

--- Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundEditor.cpp
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundEditor/Private/MetasoundEditor.cpp
@@ -1947,7 +1947,8 @@ namespace Metasound
 				.OnClosedCallback(InOnMenuClosed);
 // 				.OnCloseReason(this, &FEditor::OnGraphActionMenuClosed);
 
-			return FActionMenuContent(ActionMenu, ActionMenu->GetFilterTextBox());
+			TSharedPtr<SWidget> FilterTextBox = StaticCastSharedRef<SWidget>(ActionMenu->GetFilterTextBox());
+			return FActionMenuContent(StaticCastSharedRef<SWidget>(ActionMenu), FilterTextBox);
 		}
 
 		void FEditor::OnActionSelected(const TArray<TSharedPtr<FEdGraphSchemaAction>>& InActions, ESelectInfo::Type InSelectionType)
