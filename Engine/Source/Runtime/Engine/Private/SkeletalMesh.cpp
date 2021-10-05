6fc4fccb95026025c39cf67845b164b2b729be9c
Alexis Matte <alexis.matte@epicgames.com>
2021-10-05 11:28:58 -0400

    Fix a crash with cloth mapping data #jira UE-127668 #rb kriss.gossart #rnx #preflight 615c57e79a6de500010ac7e3
    #ROBOMERGE-OWNER: Alexis.Matte
    #ROBOMERGE-AUTHOR: alexis.matte
    #ROBOMERGE-SOURCE: CL 17721927 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v879-17706426)
    #ROBOMERGE-CONFLICT from-shelf
    #ROBOMERGE[STARSHIP]: UE5-Release-Engine-Test UE5-Main
    
    [CL 17722132 by Alexis Matte in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
+++ Engine/Source/Runtime/Engine/Private/SkeletalMesh.cpp
@@ -2493,6 +2493,7 @@ void USkeletalMesh::RemoveLegacyClothingSections()
 						if (FSkelMeshSourceSectionUserData* SectionUserData = LodModel.UserSectionsData.Find(Section.OriginalDataSectionIndex))
 						{
 							SectionUserData->CorrespondClothAssetIndex = Section.CorrespondClothAssetIndex;
+							SectionUserData->ClothingData = Section.ClothingData;
 						}
 
 						ClothingSectionCount++;
@@ -2707,8 +2708,7 @@ void USkeletalMesh::CreateUserSectionsDataForLegacyAssets()
 			bMustUseReductionSourceData = false;
 		}
 
-		const int32 NumberOfClothAssets = GetMeshClothingAssets().Num();
-		ThisLODModel.UpdateChunkedSectionInfo(GetName(), NumberOfClothAssets);
+		ThisLODModel.UpdateChunkedSectionInfo(GetName());
 
 		if (bMustUseReductionSourceData)
 		{
