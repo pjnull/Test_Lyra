de3266c453af4ba1f28f74d5f7c8488f4cae8776
stu mckenna <Stu.McKenna@epicgames.com>
2021-10-08 10:22:50 -0400

    - Add instance & particle transform space - This space allows you to transform in / out of mesh particle & instanced static meshes - Particle mesh space is now hidden and will be removed later on as it doesn't offer anything over the new space
    #rb rob.krajcarski
    
    #ROBOMERGE-AUTHOR: stu.mckenna
    #ROBOMERGE-SOURCE: CL 17754386 via CL 17754394 via CL 17754402 via CL 17754406
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v879-17706426)
    #ROBOMERGE[STARSHIP]: UE5-Release-Engine-Test UE5-Main
    
    [CL 17760327 by stu mckenna in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialExpressions.cpp
@@ -10402,7 +10402,8 @@ static EMaterialCommonBasis GetMaterialCommonBasis(EMaterialVectorCoordTransform
 		MCB_World,						// TRANSFORMSOURCE_World
 		MCB_View,						// TRANSFORMSOURCE_View
 		MCB_Camera,						// TRANSFORMSOURCE_Camera
-		MCB_MeshParticle,
+		MCB_MeshParticle,				// TRANSFORMSOURCE_Particle
+		MCB_Instance,					// TRANSFORMSOURCE_Instance
 	};
 	return ConversionTable[X];
 }
@@ -10415,7 +10416,8 @@ static EMaterialCommonBasis GetMaterialCommonBasis(EMaterialVectorCoordTransform
 		MCB_World,						// TRANSFORM_World
 		MCB_View,						// TRANSFORM_View
 		MCB_Camera,						// TRANSFORM_Camera
-		MCB_MeshParticle,
+		MCB_MeshParticle,				// TRANSFORM_Particle
+		MCB_Instance,					// TRANSFORM_Instance
 	};
 	return ConversionTable[X];
 }
@@ -10517,7 +10519,8 @@ static EMaterialCommonBasis GetMaterialCommonBasis(EMaterialPositionTransformSou
 		MCB_TranslatedWorld,			// TRANSFORMPOSSOURCE_TranslatedWorld
 		MCB_View,						// TRANSFORMPOSSOURCE_View
 		MCB_Camera,						// TRANSFORMPOSSOURCE_Camera
-		MCB_MeshParticle,	
+		MCB_MeshParticle,				// TRANSFORMPOSSOURCE_Particle
+		MCB_Instance,					// TRANSFORMPOSSOURCE_Instance
 	};
 	return ConversionTable[X];
 }
