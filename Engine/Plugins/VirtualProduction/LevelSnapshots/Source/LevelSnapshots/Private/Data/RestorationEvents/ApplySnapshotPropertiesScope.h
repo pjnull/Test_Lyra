427e6ea99d9f65701a68af6c2e852d603c542feb
dominik peacock <dominik@geodesic.games>
2021-10-18 13:18:51 -0400

    Remove duplicate slash
    #jira none
    #rb none
    
    #ROBOMERGE-AUTHOR: dominik.peacock
    #ROBOMERGE-SOURCE: CL 17847900 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v883-17842818)
    
    [CL 17847965 by dominik peacock in ue5-release-engine-staging branch]

--- Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Data/RestorationEvents/ApplySnapshotPropertiesScope.h
+++ Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Data/RestorationEvents/ApplySnapshotPropertiesScope.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Interfaces//IRestorationListener.h"
+#include "Interfaces/IRestorationListener.h"
 #include "Templates/UnrealTemplate.h"
 
 struct FPropertySelectionMap;
