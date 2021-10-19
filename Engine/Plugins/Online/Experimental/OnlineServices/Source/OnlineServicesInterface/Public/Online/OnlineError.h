e3f00e96290680c53b257a80b33a02f7b63d21ef
geoff evans <geoff.evans@epicgames.com>
2021-10-18 20:37:20 -0400

    Roll back changes in [at]17853251
    #ROBOMERGE-AUTHOR: geoff.evans
    #ROBOMERGE-SOURCE: CL 17854481 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v883-17842818)
    
    [CL 17854486 by geoff evans in ue5-release-engine-staging branch]

--- Engine/Plugins/Online/Experimental/OnlineServices/Source/OnlineServicesInterface/Public/Online/OnlineError.h
+++ Engine/Plugins/Online/Experimental/OnlineServices/Source/OnlineServicesInterface/Public/Online/OnlineError.h
@@ -1,156 +1,28 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+
 #pragma once
 
-#include "Templates/SharedPointer.h"
-#include "OnlineErrorCode.h"
-#include "CoreMinimal.h"
+/**
+ * This file is a placeholder, FOnlineError will be implemented in the future
+ */
 
-namespace UE::Online::Errors {
-
-using ErrorCodeType = uint64;
-class FOnlineError;
-#define LOCTEXT_NAMESPACE "OSSError"
-
-// Implementations of this would contain any contextual information about the error (eg, if an FText or FString exposed some fields to be filled in at run time), and likely the platform specific error representation (eg, NSError on apple that we can use to get a string to display to user and a string to log)
-class IOnlineErrorDetails
-{
-public:
-	virtual FText GetText(const FOnlineError&) const = 0;
-	virtual FString GetLogString(const FOnlineError&) const = 0;
-};
-
-class FOnlineErrorDetails : public IOnlineErrorDetails
-{
-public:
-	FOnlineErrorDetails(FString&& InLogString, FText&& InText)
-		: LogString(MoveTemp(InLogString))
-		, Text(MoveTemp(InText))
-	{
-	}
-
-	virtual ~FOnlineErrorDetails() {}
-
-	virtual FString GetLogString(const FOnlineError&) const override
-	{
-		return LogString;
-	}
-
-	virtual FText GetText(const FOnlineError&) const override
-	{
-		return Text;
-	}
-
-protected:
-	FString LogString;
-	FText Text;
-};
-
-
-
+namespace UE::Online {
 
 class FOnlineError
 {
 public:
-	FOnlineError(ErrorCodeType InErrorCode, TSharedPtr<const IOnlineErrorDetails, ESPMode::ThreadSafe> InDetails = nullptr, TSharedPtr<const FOnlineError, ESPMode::ThreadSafe> InInner = nullptr)
-		: Details(InDetails)
-		, Inner(InInner)
-		, ErrorCode(InErrorCode)
-	{
-	}
+	FOnlineError() = default;
+	FOnlineError(FString&& InError) : Error(MoveTemp(InError)) {}
+	bool operator==(const FOnlineError& Other) const { return Error == Other.Error; }
 
-
-	FText GetText() const
-	{
-		if (Details)
-		{
-			return Details->GetText(*this); // alternatively could be done via lookup on an error registry
-		}
-		else if (ErrorCode == ErrorCode::Success)
-		{
-			return LOCTEXT("Success", "Success"); // success message
-		}
-		else
-		{
-			return FText::FromString(GetErrorId()); // generic error message with code
-		}
-	}
-
-	FString GetLogString(bool bIncludePrefix = true) const
-	{
-		if (Details)
-		{
-			// likely also print out the error code first, with Details->GetLogString only providing additional information like a human readable string
-			FString LogPrefix = TEXT("");
-			if(bIncludePrefix)
-			{
-				LogPrefix = FString::Printf(TEXT("Error [%s]: "), *GetErrorId());
-			}
-
-			FString LogString = Details->GetLogString(*this);
-			return Details->GetLogString(*this);
-		}
-		else if (ErrorCode == ErrorCode::Success)
-		{
-			return TEXT("Success");
-		}
-		else
-		{
-			return FString::Printf(TEXT("Error %x"), ErrorCode); // would use %s here and below if we were to use a string ErrorCodeType
-		}
-	}
-
-	FString GetErrorId() const
-	{
-		FString ErrorId = FString::Printf(TEXT("%s"), *ErrorCode::ToString(ErrorCode));
-
-		const FOnlineError* LoopInner = GetInner();
-		while (LoopInner != nullptr)
-		{
-			ErrorId += FString::Printf(TEXT("-%s"), *ErrorCode::ToString(LoopInner->ErrorCode));
-			LoopInner = LoopInner->GetInner();
-		}
-
-		return ErrorId;
-	}
-
-	const FOnlineError* GetInner() const
-	{
-		return Inner.IsValid() ? Inner.Get() : nullptr;
-	}
-
-	ErrorCodeType GetSystem() const
-	{
-		return ErrorCodeSystem(ErrorCode);
-	}
-
-	ErrorCodeType GetCategory() const
-	{
-		return ErrorCodeCategory(ErrorCode);
-	}
-
-	ErrorCodeType GetValue() const
-	{
-		return ErrorCodeValue(ErrorCode);
-	}
-
-	ErrorCodeType GetErrorCode() const
-	{
-		return ErrorCode;
-	}
-
-private:
-	// TSharedPtr instead of TUniquePtr so that we can copy errors easily
-	TSharedPtr<const IOnlineErrorDetails, ESPMode::ThreadSafe> Details;
-	TSharedPtr<const FOnlineError, ESPMode::ThreadSafe> Inner;
-	ErrorCodeType ErrorCode;
+	FString Error;
 };
 
+namespace Errors
+{
+	inline FOnlineError Cancelled() { return FOnlineError(); }
+	inline FOnlineError Unimplemented() { return FOnlineError(); }
+	inline FOnlineError UnknownError() { return FOnlineError(); }
+}
 
-
-// the comparison with ErrorCodeType is the one I would expect to see in almost every case
-bool operator==(const FOnlineError& Lhs, const FOnlineError& Rhs);
-bool operator==(const FOnlineError& Lhs, ErrorCodeType OtherErrorCode);
-
-} // namespace UE::Online::Errors
-
-#undef LOCTEXT_NAMESPACE
+/* UE::Online */ }
