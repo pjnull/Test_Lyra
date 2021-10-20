dad4bbd191cd2a04d70608bec7f181cd283b7f5d
juan canada <Juan.Canada@epicgames.com>
2021-10-20 10:36:09 -0400

    Fixed compile error
    #rb trivial
    #jira none
    #horde 109527
    
    #ROBOMERGE-AUTHOR: juan.canada
    #ROBOMERGE-SOURCE: CL 17873122 in //UE5/Main/... via CL 17873145
    #ROBOMERGE-BOT: STARSHIP (Release-Engine-Test -> Release-Engine-Staging) (v883-17842818)
    #ROBOMERGE[STARSHIP]: Release-5.0
    
    [CL 17873155 by juan canada in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -4770,6 +4770,7 @@ bool UEngine::HandleGameVerCommand( const TCHAR* Cmd, FOutputDevice& Ar )
 
 bool UEngine::HandleStatCommand( UWorld* World, FCommonViewportClient* ViewportClient, const TCHAR* Cmd, FOutputDevice& Ar )
 {
+#if STATS
 	if (FParse::Command(&Cmd, TEXT("help")))
 	{
 		const TSet<FName>& StatGroupNames = FStatGroupGameThreadNotifier::Get().StatGroupNames;
@@ -4795,6 +4796,7 @@ bool UEngine::HandleStatCommand( UWorld* World, FCommonViewportClient* ViewportC
 
 		return true;
 	}
+#endif //STATS
 
 	const TCHAR* Temp = Cmd;
 	for (int32 StatIdx = 0; StatIdx < EngineStats.Num(); StatIdx++)
