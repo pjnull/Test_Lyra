215eded333d4f3e20e0cf758e3181d85af69a595
matt peters <Matt.Peters@epicgames.com>
2021-10-26 09:49:23 -0400

    TopologicalSort: Add function comment. #rb Danny.Couture #rnx
    #ROBOMERGE-AUTHOR: matt.peters
    #ROBOMERGE-SOURCE: CL 17923119 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v885-17909292)
    
    [CL 17923129 by matt peters in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Core/Public/Algo/TopologicalSort.h
+++ Engine/Source/Runtime/Core/Public/Algo/TopologicalSort.h
@@ -6,6 +6,15 @@
 
 namespace Algo
 {
+	/**
+	 * Sorts the given range in root to leaf order.
+	 *
+	 * @param UniqueRange A range with element type T
+	 * @param GetElementDependencies A callable with prototype that is one of
+	 *            RangeType<T> GetElementDependencies(const T& Element)
+	 *            const RangeType<T>& GetElementDependencies(const T& Element)
+	 *        It should return the leaf-ward vertices of directed edges from the root-wards Element.
+	 */
 	template <typename RangeType, typename GetElementDependenciesType>
 	FORCEINLINE bool TopologicalSort(RangeType& UniqueRange, GetElementDependenciesType GetElementDependencies)
 	{
