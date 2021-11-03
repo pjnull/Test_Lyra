fd344eece1200f8fa01d6f3a75112c51b27074ad
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-11-03 18:08:21 -0400

    Fix for disabled shader stages contributing to compilation (at least at the graph level). #rb stu.mckenna #jira UE-133454
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 18042078 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v885-17909292)
    
    [CL 18042094 by rob krajcarski in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraEmitter.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraEmitter.cpp
@@ -1121,9 +1121,10 @@ void UNiagaraEmitter::GetScripts(TArray<UNiagaraScript*>& OutScripts, bool bComp
 		{
 			if (SimulationStages[i] && SimulationStages[i]->Script)
 			{
-				//if (bEnabledOnly && (!SimulationStages[i]->bEnabled || !bSimulationStagesEnabled))
-				//	continue;
-				OutScripts.Add(SimulationStages[i]->Script);
+				if (!bEnabledOnly || SimulationStages[i]->bEnabled)
+				{
+					OutScripts.Add(SimulationStages[i]->Script);
+				}
 			}
 		}
 	}
