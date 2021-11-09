5972728b890a0aac6fc537ed67fff4e90741fa79
miles flanagan <miles.flanagan@psyonix.com>
2021-11-09 14:53:27 -0500

    Move commonly used EEnvelopePeakMode metasound enum out of metasound nodes where it was being duplicated.
    #rb rob.gay
    #jira none
    
    #ROBOMERGE-AUTHOR: miles.flanagan
    #ROBOMERGE-SOURCE: CL 18110809 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18110847 by miles flanagan in ue5-release-engine-staging branch]

---
+++ Engine/Plugins/Runtime/Metasound/Source/MetasoundStandardNodes/Public/MetasoundEnvelopeFollowerTypes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MetasoundFacade.h"
+#include "MetasoundEnumRegistrationMacro.h"
+
+namespace Metasound
+{
+	enum class EEnvelopePeakMode
+	{
+		Peak = 0,
+		MeanSquared,
+		RootMeanSquared,
+	};
+
+	DECLARE_METASOUND_ENUM(EEnvelopePeakMode, EEnvelopePeakMode::Peak, METASOUNDSTANDARDNODES_API,
+	FEnumEnvelopePeakMode, FEnumEnvelopePeakModeInfo, FEnvelopePeakModeReadRef, FEnumEnvelopePeakModeWriteRef);
+}
