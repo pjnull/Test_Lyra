df7825a06fdf0b19e386e5b6be4cdf0174128762
cedric caillaud <cedric.caillaud@epicgames.com>
2021-11-10 17:57:04 -0500

    optimize Heightfield ContactManifoldImp by checking the cell bounds against the query bounds before calling expensive code against it
    #rb zhenglin.geng, ori.cohen, chris.caulfield
    #jira none
    
    #ROBOMERGE-AUTHOR: cedric.caillaud
    #ROBOMERGE-SOURCE: CL 18140289 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18140329 by cedric caillaud in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/HeightField.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/HeightField.cpp
@@ -1352,23 +1352,27 @@ namespace Chaos
 			}
 
 			// The triangle is solid so proceed to test it
-			GeomData.GetPointsScaled(SingleIndex, Points);
-			// First Triangle
+			FAABB3 CellBounds;
+			GeomData.GetPointsAndBoundsScaled(SingleIndex, Points, CellBounds);
+			if (CellBounds.Intersects(QueryBounds))
 			{
-				FPBDCollisionConstraint Constraint;
-				OverlapTriangle(Points[0], Points[1], Points[3], Constraint);
-				for (const FManifoldPoint& ManifoldPoint : Constraint.GetManifoldPoints())
+				// First Triangle
 				{
-					InsertSorted(ManifoldPoint.ContactPoint);
+					FPBDCollisionConstraint Constraint;
+					OverlapTriangle(Points[0], Points[1], Points[3], Constraint);
+					for (const FManifoldPoint& ManifoldPoint : Constraint.GetManifoldPoints())
+					{
+						InsertSorted(ManifoldPoint.ContactPoint);
+					}
 				}
-			}
-			// Second Triangle
-			{
-				FPBDCollisionConstraint Constraint;
-				OverlapTriangle(Points[0], Points[3], Points[2], Constraint);
-				for (const FManifoldPoint& ManifoldPoint : Constraint.GetManifoldPoints())
+				// Second Triangle
 				{
-					InsertSorted(ManifoldPoint.ContactPoint);
+					FPBDCollisionConstraint Constraint;
+					OverlapTriangle(Points[0], Points[3], Points[2], Constraint);
+					for (const FManifoldPoint& ManifoldPoint : Constraint.GetManifoldPoints())
+					{
+						InsertSorted(ManifoldPoint.ContactPoint);
+					}
 				}
 			}
 		}
