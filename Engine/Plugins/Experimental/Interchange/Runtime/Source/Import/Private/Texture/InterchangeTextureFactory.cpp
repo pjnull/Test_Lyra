316849e0b5ecd2a63155d993b30d1d97f26c0516
julien stjean <Julien.StJean@epicgames.com>
2021-11-10 12:54:07 -0500

    Fixes different default settings when importing a light profile from interchange vs the legacy factory.
    #rb Alexis.Matte
    #jira UE-133329
    #preflight 618becebda4267a14dc42dc9
    
    #ROBOMERGE-AUTHOR: julien.stjean
    #ROBOMERGE-SOURCE: CL 18131201 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18132985 by julien stjean in ue5-release-engine-staging branch]

--- Engine/Plugins/Experimental/Interchange/Runtime/Source/Import/Private/Texture/InterchangeTextureFactory.cpp
+++ Engine/Plugins/Experimental/Interchange/Runtime/Source/Import/Private/Texture/InterchangeTextureFactory.cpp
@@ -8,6 +8,7 @@
 #include "Engine/Texture2D.h"
 #include "Engine/Texture2DArray.h"
 #include "Engine/TextureCube.h"
+#include "Engine/TextureDefines.h"
 #include "Engine/TextureLightProfile.h"
 #include "HAL/FileManagerGeneric.h"
 #include "InterchangeAssetImportData.h"
@@ -808,6 +809,14 @@ UObject* UInterchangeTextureFactory::CreateEmptyAsset(const FCreateAssetParams& 
 		UTexture* NewTexture = NewObject<UTexture>(Arguments.Parent, TextureClass, *Arguments.AssetName, RF_Public | RF_Standalone);
 		NewTexture->AlphaCoverageThresholds = FVector4(0.0, 0.0, 0.0, 1.0);
 		Texture = NewTexture;
+
+		if (UTextureLightProfile* LightProfile = Cast<UTextureLightProfile>(NewTexture))
+		{
+			LightProfile->AddressX = TA_Clamp;
+			LightProfile->AddressY = TA_Clamp;
+			LightProfile->MipGenSettings = TMGS_NoMipmaps;
+			LightProfile->LODGroup = TEXTUREGROUP_IESLightProfile;
+		}
 	}
 	else if (ExistingAsset->GetClass()->IsChildOf(TextureClass))
 	{
