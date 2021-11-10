75885c899f493559acafe9ef503930fb0a9ba194
aditya ravichandran <aditya.ravichandran@epicgames.com>
2021-11-09 22:37:19 -0500

    Fix crash and warnings in Scene Outliner due to misplaced nullptr check #rb trivial
    #ROBOMERGE-AUTHOR: aditya.ravichandran
    #ROBOMERGE-SOURCE: CL 18122261 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18122663 by aditya ravichandran in ue5-release-engine-staging branch]

--- Engine/Source/Editor/SceneOutliner/Private/SSceneOutliner.cpp
+++ Engine/Source/Editor/SceneOutliner/Private/SSceneOutliner.cpp
@@ -276,10 +276,11 @@ void SSceneOutliner::HandleHiddenColumnsChanged()
 {
 	const TArray<FName> HiddenColumns = HeaderRowWidget->GetHiddenColumnIds();
 	FSceneOutlinerConfig* OutlinerConfig = GetMutableConfig();
-	TMap<FName, bool> ColumnVisibilities = OutlinerConfig->ColumnVisibilities;
 
 	if (OutlinerConfig != nullptr)
 	{
+		TMap<FName, bool> ColumnVisibilities = OutlinerConfig->ColumnVisibilities;
+
 		for (const TPair<FName, bool> Pair : Columns)
 		{
 			ColumnVisibilities.Add(Pair.Key, HiddenColumns.Find(Pair.Key) == INDEX_NONE);
