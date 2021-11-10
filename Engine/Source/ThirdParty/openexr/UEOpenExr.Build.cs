55741963e845308a5fb1d1b8ee1d81552c1ce31f
matt johnson <matt.johnson@epicgames.com>
2021-11-09 20:19:22 -0500

    OpenEXR: add build scripts and build products for OpenEXR version 3.1.1
    Though this change does add the relevant code to the module rules to support
    OpenEXR 3.1.1, it is *not* yet activated here. There are some additional code
    changes needed elsewhere to adapt to the new version of OpenEXR, so a
    subsequent change will include those updates along with the switch in the
    module rules to move to OpenEXR 3.1.1 and remove OpenEXR 2.3.0.
    
    #jira UE-131389
    #rb ruslan.idrisov, brandon.schaefer
    
    #ROBOMERGE-AUTHOR: matt.johnson
    #ROBOMERGE-SOURCE: CL 18119345 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18119525 by matt johnson in ue5-release-engine-staging branch]

--- Engine/Source/ThirdParty/openexr/UEOpenExr.Build.cs
+++ Engine/Source/ThirdParty/openexr/UEOpenExr.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class UEOpenExr : ModuleRules
@@ -7,22 +8,95 @@ public class UEOpenExr : ModuleRules
 	private enum EXRVersion
 	{
 		EXR_2_3_0,
+		EXR_3_1_1
 	}
 	public UEOpenExr(ReadOnlyTargetRules Target) : base(Target)
 	{
+		Type = ModuleType.External;
+
+		bool bDebug = (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT);
+
+		string DeploymentDirectory = Path.Combine(ModuleDirectory, "Deploy", "openexr-3.1.1");
+
 		// Set which version to use.
+		// XXX: Soon to be switched to 3.1.1, and 2.3.0 (and the enum) removed...
 		EXRVersion Version = EXRVersion.EXR_2_3_0;
 
-		string DeployDir = "openexr/Deploy/";
-		if (Version == EXRVersion.EXR_2_3_0)
+		if (Version >= EXRVersion.EXR_3_1_1)
 		{
-			DeployDir += "OpenEXR-2.3.0";
+			PublicDependencyModuleNames.Add("Imath");
+
+			PublicIncludePaths.Add(Path.Combine(DeploymentDirectory, "include"));
+
+			// XXX: OpenEXR includes some of its own headers without the
+			// leading "OpenEXR/..."
+			PublicIncludePaths.Add(Path.Combine(DeploymentDirectory, "include", "OpenEXR"));
+
+			string LibPostfix = bDebug ? "_d" : "";
+
+			if (Target.IsInPlatformGroup(UnrealPlatformGroup.Windows))
+			{
+				string LibDirectory = Path.Combine(
+					DeploymentDirectory,
+					"VS" + Target.WindowsPlatform.GetVisualStudioCompilerVersionName(),
+					Target.WindowsPlatform.GetArchitectureSubpath(),
+					"lib");
+
+				PublicAdditionalLibraries.AddRange(
+					new string[] {
+						Path.Combine(LibDirectory, "Iex-3_1" + LibPostfix + ".lib"),
+						Path.Combine(LibDirectory, "IlmThread-3_1" + LibPostfix + ".lib"),
+						Path.Combine(LibDirectory, "OpenEXR-3_1" + LibPostfix + ".lib"),
+						Path.Combine(LibDirectory, "OpenEXRCore-3_1" + LibPostfix + ".lib"),
+						Path.Combine(LibDirectory, "OpenEXRUtil-3_1" + LibPostfix + ".lib")
+					}
+				);
+			}
+			else if (Target.Platform == UnrealTargetPlatform.Mac)
+			{
+				string LibDirectory = Path.Combine(
+					DeploymentDirectory,
+					"Mac",
+					"lib");
+
+				PublicAdditionalLibraries.AddRange(
+					new string[] {
+						Path.Combine(LibDirectory, "libIex-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libIlmThread-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXR-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXRCore-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXRUtil-3_1" + LibPostfix + ".a")
+					}
+				);
+			}
+			else if (Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
+			{
+				string LibDirectory = Path.Combine(
+					DeploymentDirectory,
+					"Unix",
+					Target.Architecture,
+					"lib");
+
+				PublicAdditionalLibraries.AddRange(
+					new string[] {
+						Path.Combine(LibDirectory, "libIex-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libIlmThread-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXR-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXRCore-3_1" + LibPostfix + ".a"),
+						Path.Combine(LibDirectory, "libOpenEXRUtil-3_1" + LibPostfix + ".a")
+					}
+				);
+			}
+
+			return;
 		}
 
-		Type = ModuleType.External;
+		// Legacy OpenEXR version below, soon to be removed.
+
+		string DeployDir = Path.Combine("openexr", "Deploy", "OpenEXR-2.3.0");
+
 		if (Target.Platform.IsInGroup(UnrealPlatformGroup.Windows) || Target.Platform == UnrealTargetPlatform.Mac || Target.IsInPlatformGroup(UnrealPlatformGroup.Unix))
 		{
-			bool bDebug = (Target.Configuration == UnrealTargetConfiguration.Debug && Target.bDebugBuildsActuallyUseDebugCRT);
 			string LibDir = Target.UEThirdPartySourceDirectory + DeployDir + "/OpenEXR//lib/";
 			string Platform = "";
 			if (Target.Platform.IsInGroup(UnrealPlatformGroup.Windows))
