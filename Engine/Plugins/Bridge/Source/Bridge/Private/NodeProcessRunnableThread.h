932d1b9b8d283ec35653af496cae70305cf50fe7
durra mohsin <durra@epicgames.quixel.com>
2021-11-11 04:42:50 -0500

    Fix non-unity issue in Bridge Issue #116550
    #ROBOMERGE-AUTHOR: durra.mohsin
    #ROBOMERGE-SOURCE: CL 18147739 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18147747 by durra mohsin in ue5-release-engine-staging branch]

--- Engine/Plugins/Bridge/Source/Bridge/Private/NodeProcessRunnableThread.h
+++ Engine/Plugins/Bridge/Source/Bridge/Private/NodeProcessRunnableThread.h
@@ -1,6 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "CoreMinimal.h"
+#include "HAL/Runnable.h"
+#include "HAL/PlatformProcess.h"
 
 class NodeProcessRunnableThread : public FRunnable
 {
