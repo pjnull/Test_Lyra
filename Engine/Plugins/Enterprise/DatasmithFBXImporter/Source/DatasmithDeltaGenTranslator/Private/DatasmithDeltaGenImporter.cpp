0d2d6109c928d340b787733ccb4020e1f237e7d2
daniel coelho <daniel.coelho@xa.epicgames.com>
2021-11-11 06:48:13 -0500

    VRED/DeltaGen: Set materials directly on created static meshes as well as using them as material overrides
    #jira UE-127918
    #rb JeanLuc.Corenthin
    
    #ROBOMERGE-AUTHOR: daniel.coelho
    #ROBOMERGE-SOURCE: CL 18148962 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18148999 by daniel coelho in ue5-release-engine-staging branch]

--- Engine/Plugins/Enterprise/DatasmithFBXImporter/Source/DatasmithDeltaGenTranslator/Private/DatasmithDeltaGenImporter.cpp
+++ Engine/Plugins/Enterprise/DatasmithFBXImporter/Source/DatasmithDeltaGenTranslator/Private/DatasmithDeltaGenImporter.cpp
@@ -281,6 +281,7 @@ TSharedPtr<IDatasmithActorElement> FDatasmithDeltaGenImporter::ConvertNode(const
 		TSharedPtr<FDatasmithFBXSceneMesh> ThisMesh = Node->Mesh;
 		FName MeshName = FName(*ThisMesh->Name);
 
+		TSharedPtr<IDatasmithMeshElement> CreatedMesh = nullptr;
 		TSharedPtr<FDatasmithFBXSceneMesh>* FoundMesh = MeshNameToFBXMesh.Find(MeshName);
 		if (FoundMesh && (*FoundMesh).IsValid())
 		{
@@ -291,7 +292,7 @@ TSharedPtr<IDatasmithActorElement> FDatasmithDeltaGenImporter::ConvertNode(const
 		{
 			// Create a mesh
 			MeshNameToFBXMesh.Add(MeshName, ThisMesh);
-			TSharedRef<IDatasmithMeshElement> MeshElement = FDatasmithSceneFactory::CreateMesh(*ThisMesh->Name);
+			CreatedMesh = FDatasmithSceneFactory::CreateMesh(*ThisMesh->Name);
 
 			FMeshDescription& MeshDescription = ThisMesh->MeshDescription;
 			FStaticMeshAttributes StaticMeshAttributes(MeshDescription);
@@ -302,10 +303,10 @@ TSharedPtr<IDatasmithActorElement> FDatasmithDeltaGenImporter::ConvertNode(const
 			// Don't set it to zero or else it will disable Datasmith's GenerateLightmapUV option
 			if (NumUVChannels > 1)
 			{
-				MeshElement->SetLightmapCoordinateIndex(1);
+				CreatedMesh->SetLightmapCoordinateIndex(1);
 			}
 
-			DatasmithScene->AddMesh(MeshElement);
+			DatasmithScene->AddMesh(CreatedMesh);
 		}
 
 		TSharedPtr<IDatasmithMeshActorElement> MeshActorElement = FDatasmithSceneFactory::CreateMeshActor(*Node->Name);
@@ -319,6 +320,12 @@ TSharedPtr<IDatasmithActorElement> FDatasmithDeltaGenImporter::ConvertNode(const
 			TSharedRef<IDatasmithMaterialIDElement> MaterialIDElement(FDatasmithSceneFactory::CreateMaterialId(MaterialElement->GetName()));
 			MaterialIDElement->SetId(MaterialID);
 			MeshActorElement->AddMaterialOverride(MaterialIDElement);
+
+			// Also set the material directly on the mesh if this was the node that created it
+			if (CreatedMesh)
+			{
+				CreatedMesh->SetMaterial(MaterialElement->GetName(), MaterialID);
+			}
 		}
 
 		ActorElement = MeshActorElement;
