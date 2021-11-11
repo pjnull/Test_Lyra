fdfd568fc08d14e3386f8d5c53ad48d02c9d5453
dominik peacock <dominik@geodesic.games>
2021-11-11 16:29:41 -0500

    Fix compiler warning
    #jira none
    #rb none
    
    #ROBOMERGE-AUTHOR: dominik.peacock
    #ROBOMERGE-SOURCE: CL 18158699 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18158728 by dominik peacock in ue5-release-engine-staging branch]

--- Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Selection/PropertySelectionMap.cpp
+++ Engine/Plugins/VirtualProduction/LevelSnapshots/Source/LevelSnapshots/Private/Selection/PropertySelectionMap.cpp
@@ -43,17 +43,17 @@ bool FPropertySelectionMap::AddObjectProperties(UObject* WorldObject, const FPro
 
 void FPropertySelectionMap::MarkSubobjectForRestoringReferencesButSkipProperties(UObject* WorldSubbject)
 {
-	if (UActorComponent* Component = Cast<UActorComponent>(WorldSubbject))
-	{
-		ensureAlwaysMsgf(Component->CreationMethod == EComponentCreationMethod::Native || Component->CreationMethod == EComponentCreationMethod::UserConstructionScript,
-			TEXT("Native components are expected to wind-up here if they're not referenced by uproperties (see FComponentEditorUtils::CanEditComponentInstance). Instanced and SimpleUserConstructionScriptComponent should never show up here."));
-		UE_CLOG(WorldSubbject->IsA<UActorComponent>(), LogLevelSnapshots, Warning, TEXT("Reference to component %s will not be restored. Note: native component are only restored if they're referenced by an EditAnywhere property. See FComponentEditorUtils::CanEditComponentInstance."));
-		return;
-	}
-
 	if (ensure(WorldSubbject)
 		&& ensureMsgf(!EditorWorldObjectToSelectedProperties.Contains(WorldSubbject), TEXT("Object was already added via AddObjectProperties with changed properties. Now we're told that is has no changed properties...")))
 	{
+		if (UActorComponent* Component = Cast<UActorComponent>(WorldSubbject))
+		{
+			ensureAlwaysMsgf(Component->CreationMethod == EComponentCreationMethod::Native || Component->CreationMethod == EComponentCreationMethod::UserConstructionScript,
+				TEXT("Native components are expected to wind-up here if they're not referenced by uproperties (see FComponentEditorUtils::CanEditComponentInstance). Instanced and SimpleUserConstructionScriptComponent should never show up here."));
+			UE_LOG(LogLevelSnapshots, Warning, TEXT("Reference to component %s will not be restored. Note: native component are only restored if they're referenced by an EditAnywhere property. See FComponentEditorUtils::CanEditComponentInstance."), *WorldSubbject->GetPathName());
+			return;
+		}
+		
 		SubobjectsMarkedForReferencesRestorationOnly.Add(WorldSubbject);
 	}
 }
