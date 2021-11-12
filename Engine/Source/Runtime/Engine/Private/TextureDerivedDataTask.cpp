aab81de81dc3ab80293c4f3ff3362ae92ced7c99
jeremy moore <Jeremy.Moore@epicgames.com>
2021-11-11 19:16:34 -0500

    Convert warnings to logs for cases where we pass virtual textures with blocks and layers to cooking if VT is disabled. In this case we will cook a single layer or block.
    #ROBOMERGE-AUTHOR: jeremy.moore
    #ROBOMERGE-SOURCE: CL 18139626 via CL 18140881 via CL 18146052 via CL 18152402 via CL 18153655 via CL 18161439
    #ROBOMERGE-BOT: STARSHIP (Release-5.0 -> Release-Engine-Staging) (v889-18060218)
    
    [CL 18161756 by jeremy moore in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/TextureDerivedDataTask.cpp
+++ Engine/Source/Runtime/Engine/Private/TextureDerivedDataTask.cpp
@@ -470,15 +470,14 @@ void FTextureCacheDerivedDataWorker::BuildTexture(TArray<FTextureBuildSettings>&
 		// Only support single Block/Layer here (Blocks and Layers are intended for VT support)
 		if (TextureData.Blocks.Num() > 1)
 		{
-			// This warning can happen if user attempts to import a UDIM without VT enabled
-			UE_LOG(LogTexture, Warning, TEXT("Texture %s was imported as UDIM with %d blocks but VirtualTexturing is not enabled, only the first block will be available"),
+			// This can happen if user attempts to import a UDIM without VT enabled
+			UE_LOG(LogTexture, Log, TEXT("Texture %s was imported as UDIM with %d blocks but VirtualTexturing is not enabled, only the first block will be available"),
 				*TexturePathName, TextureData.Blocks.Num());
 		}
-
-		// No user-facing way to generated multi-layered textures currently, so this should not occur
 		if (TextureData.Layers.Num() > 1)
 		{
-			UE_LOG(LogTexture, Warning, TEXT("Texture %s has %d layers but VirtualTexturing is not enabled, only the first layer will be available"),
+			// This can happen if user attempts to use lightmaps or other layered VT without VT enabled
+			UE_LOG(LogTexture, Log, TEXT("Texture %s has %d layers but VirtualTexturing is not enabled, only the first layer will be available"),
 				*TexturePathName, TextureData.Layers.Num());
 		}
 
@@ -1276,15 +1275,14 @@ public:
 			// Only support single Block/Layer here (Blocks and Layers are intended for VT support)
 			if (NumBlocks > 1)
 			{
-				// This warning can happen if user attempts to import a UDIM without VT enabled
-				UE_LOG(LogTexture, Warning, TEXT("Texture %s was imported as UDIM with %d blocks but VirtualTexturing is not enabled, only the first block will be available"),
+				// This can happen if user attempts to import a UDIM without VT enabled
+				UE_LOG(LogTexture, Log, TEXT("Texture %s was imported as UDIM with %d blocks but VirtualTexturing is not enabled, only the first block will be available"),
 					*Texture.GetPathName(), NumBlocks);
 			}
-
-			// No user-facing way to generated multi-layered textures currently, so this should not occur
 			if (NumLayers > 1)
 			{
-				UE_LOG(LogTexture, Warning, TEXT("Texture %s has %d layers but VirtualTexturing is not enabled, only the first layer will be available"),
+				// This can happen if user attempts to use lightmaps or other layered VT without VT enabled
+				UE_LOG(LogTexture, Log, TEXT("Texture %s has %d layers but VirtualTexturing is not enabled, only the first layer will be available"),
 					*Texture.GetPathName(), NumLayers);
 			}
 		}
