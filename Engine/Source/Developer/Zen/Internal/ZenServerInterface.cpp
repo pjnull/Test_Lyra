60d42be0d1a8780578abb7ccd87f1e40809eb90a
zousar shaker <zousar.shaker@epicgames.com>
2022-02-14 11:50:31 -0500

    Fix excessive thread spawning caused by name resolving of "localhost" at high frequency by the Zen stats code. Re-use Curl request when gathering stats.  Rate limit it to no more than 2/second.  Still keeping it synchronous and blocking for now. Avoid using "localhost" and prefer the IPV6 loopback address of [::1] to avoid name resolution penalty for localhost. Keep DNS cache in Curl permanently for a given request.
    #rb devin.doucette
    #jira UE-142437
    #preflight 620a7d7b6e5d06a542963178
    
    #ROBOMERGE-AUTHOR: zousar.shaker
    #ROBOMERGE-SOURCE: CL 18980515 in //UE5/Release-5.0/...
    #ROBOMERGE-BOT: UE5 (Release-5.0 -> Release-Engine-Staging) (v917-18934589)
    
    [CL 18980843 by zousar shaker in ue5-release-engine-staging branch]

--- Engine/Source/Developer/Zen/Internal/ZenServerInterface.cpp
+++ Engine/Source/Developer/Zen/Internal/ZenServerInterface.cpp
@@ -3,7 +3,6 @@
 
 
 #include "ZenBackendUtils.h"
-#include "ZenServerHttp.h"
 #include "ZenSerialization.h"
 
 #include "Dom/JsonValue.h"
@@ -183,6 +182,11 @@ IsLocalHost(const FString& Host)
 		return true;
 	}
 
+	if (Host.Compare(FString(TEXT("[::1]"))) == 0)
+	{
+		return true;
+	}
+
 	ISocketSubsystem& SocketSubsystem = *ISocketSubsystem::Get();
 
 	const TSharedPtr<FInternetAddr> Addr = SocketSubsystem.GetAddressFromString(Host);
@@ -347,7 +351,7 @@ FServiceSettings::TryApplyAutoLaunchOverride()
 	{
 		SettingsVariant.Emplace<FServiceConnectSettings>();
 		FServiceConnectSettings& ConnectExistingSettings = SettingsVariant.Get<FServiceConnectSettings>();
-		ConnectExistingSettings.HostName = TEXT("localhost");
+		ConnectExistingSettings.HostName = TEXT("[::1]");
 		ConnectExistingSettings.Port = 1337;
 		return true;
 	}
@@ -851,7 +855,7 @@ FZenServiceInstance::AutoLaunch(const FServiceAutoLaunchSettings& InSettings, FS
 	}
 
 
-	OutHostName = TEXT("localhost");
+	OutHostName = TEXT("[::1]");
 	// Default to assuming that we get to run on the port we want
 	OutPort = DesiredPort;
 
@@ -949,9 +953,23 @@ FZenServiceInstance::AutoLaunch(const FServiceAutoLaunchSettings& InSettings, FS
 bool 
 FZenServiceInstance::GetStats( FZenStats& stats ) const
 {
+	check(IsInGameThread());
+
+	const uint64 CurrentCycles = FPlatformTime::Cycles64();
+	if (StatsRequest.IsSet() && FPlatformTime::ToSeconds(CurrentCycles - LastStatsTime) < 0.5)
+	{
+		stats = LastStats;
+		return true;
+	}
+
 	TStringBuilder<128> ZenDomain;
 	ZenDomain << HostName << TEXT(":") << Port;
-	UE::Zen::FZenHttpRequest Request(ZenDomain.ToString(), false);
+	if (!StatsRequest.IsSet())
+	{
+		StatsRequest.Emplace(ZenDomain.ToString(), false);
+	}
+	UE::Zen::FZenHttpRequest& Request = StatsRequest.GetValue();
+	Request.Reset();
 
 	TArray64<uint8> GetBuffer;
 	FZenHttpRequest::Result Result = Request.PerformBlockingDownload(TEXT("/stats/z$"_SV), &GetBuffer, Zen::EContentType::CbObject);
@@ -1037,6 +1055,8 @@ FZenServiceInstance::GetStats( FZenStats& stats ) const
 		CASSizeStats.Large = CASSizeObjectView["large"].AsInt64();
 		CASSizeStats.Total = CASSizeObjectView["total"].AsInt64();
 
+		LastStats = stats;
+		LastStatsTime = CurrentCycles;
 		return true;
 	}
 
