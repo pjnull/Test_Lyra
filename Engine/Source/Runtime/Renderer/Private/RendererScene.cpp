5e600305f3c141c475335eae49c8008592da5c26
chris kulla <chris.kulla@epicgames.com>
2022-02-23 16:44:31 -0500

    Fix path tracer updates not working over nDisplay and in the editor's Picture-in-picture camera previews.
     * Invalidate Path Traced output when component transforms change (this catches cases that don't re-create a primitive info object)
     * Invalidate Path Traced output when light color or intensity changes
     * Fix invalidation in a scene only affecting a single view (or view family). Instead of a single boolean, the scene now keeps a counter that ticks on changes and each view remembers the last value of this counter to detect changes.
    
    #jira UE-124715
    #rb Ryan.Vance,Juan.Canada
    #lockdown Juan.Canada
    #preflight 6210149c676ae0301347db71
    
    
    
    #ROBOMERGE-AUTHOR: chris.kulla
    #ROBOMERGE-SOURCE: CL 19087008 via CL 19092565 via CL 19092634 via CL 19093389
    #ROBOMERGE-BOT: (v921-19075845)
    
    [CL 19101802 by chris kulla in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
+++ Engine/Source/Runtime/Renderer/Private/RendererScene.cpp
@@ -1148,7 +1148,7 @@ FScene::FScene(UWorld* InWorld, bool bInRequiresHitProxies, bool bInIsEditorScen
 ,	FXSystem(nullptr)
 ,	NaniteRasterPipelines(InFeatureLevel)
 ,	bScenesPrimitivesNeedStaticMeshElementUpdate(false)
-,	bPathTracingNeedsInvalidation(true)
+,   PathTracingInvalidationCounter(0)
 #if RHI_RAYTRACING
 ,   CachedRayTracingMeshCommandsMode(ERayTracingMeshCommandsMode::RAY_TRACING)
 #endif
@@ -1968,6 +1968,7 @@ void FScene::AddLightSceneInfo_RenderThread(FLightSceneInfo* LightSceneInfo)
 	{
 		RayTracedLights.Add(LightSceneInfo);
 	}
+	InvalidatePathTracedOutput();
 #endif 
 
 	// Add the light to the scene.
@@ -2928,7 +2929,7 @@ void FScene::InvalidateRuntimeVirtualTexture(class URuntimeVirtualTextureCompone
 void FScene::InvalidatePathTracedOutput()
 {
 	// NOTE: this is an atomic, so this function is ok to call from any thread
-	bPathTracingNeedsInvalidation = true;
+	++PathTracingInvalidationCounter;
 }
 
 void FScene::FlushDirtyRuntimeVirtualTextures()
@@ -3074,7 +3075,7 @@ void FScene::UpdateLightColorAndBrightness(ULightComponent* Light)
 		{
 			 NewParameters.NewColor *= FLinearColor::MakeFromColorTemperature(Light->Temperature);
 		}
-	
+
 		FScene* Scene = this;
 		FLightSceneInfo* LightSceneInfo = Light->SceneProxy->GetLightSceneInfo();
 		ENQUEUE_RENDER_COMMAND(UpdateLightColorAndBrightness)(
@@ -3090,6 +3091,9 @@ void FScene::UpdateLightColorAndBrightness(ULightComponent* Light)
 						( Scene->GetShadingPath() == EShadingPath::Mobile 
 						&& NewParameters.NewColor.IsAlmostBlack() != LightSceneInfo->Proxy->GetColor().IsAlmostBlack() );
 
+					// Path Tracing: something about the light has changed, restart path traced accumulation
+					Scene->InvalidatePathTracedOutput();
+
 					LightSceneInfo->Proxy->SetColor(NewParameters.NewColor);
 					LightSceneInfo->Proxy->IndirectLightingScale = NewParameters.NewIndirectLightingScale;
 					LightSceneInfo->Proxy->VolumetricScatteringIntensity = NewParameters.NewVolumetricScatteringIntensity;
@@ -3099,7 +3103,6 @@ void FScene::UpdateLightColorAndBrightness(ULightComponent* Light)
 					{
 						Scene->Lights[ LightSceneInfo->Id ].Color = NewParameters.NewColor;
 					}
-
 					LightSceneInfo->Proxy->SetMobileMovablePointLightUniformBufferNeedsUpdate(true);
 				}
 			});
@@ -3197,6 +3200,8 @@ void FScene::RemoveLightSceneInfo_RenderThread(FLightSceneInfo* LightSceneInfo)
 		{
 			OverflowingDynamicShadowedLights.Remove(LightSceneInfo->Proxy->GetOwnerNameOrLabel());
 		}
+
+		InvalidatePathTracedOutput();
 	}
 	else
 	{
@@ -4900,9 +4905,6 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 					VelocityData.UpdateTransform(PrimitiveSceneInfo, PrimitiveTransforms[PrimitiveIndex], PrimitiveTransforms[PrimitiveIndex]);
 				}
 
-				// Invalidate PathTraced image because we added something to the scene
-				bPathTracingNeedsInvalidation = true;
-
 				DistanceFieldSceneData.AddPrimitive(PrimitiveSceneInfo);
 				LumenSceneData->AddPrimitive(PrimitiveSceneInfo);
 
@@ -4914,6 +4916,11 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 
 				// Update scene LOD tree
 				SceneLODHierarchy.UpdateNodeSceneInfo(PrimitiveSceneInfo->PrimitiveComponentId, PrimitiveSceneInfo);
+			}
+			if (StartIndex < AddedLocalPrimitiveSceneInfos.Num())
+			{
+				// Invalidate PathTraced image because we added something to the scene
+				InvalidatePathTracedOutput();
 			}
 			AddedLocalPrimitiveSceneInfos.RemoveAt(StartIndex, AddedLocalPrimitiveSceneInfos.Num() - StartIndex, false);
 		}
@@ -5198,9 +5205,11 @@ void FScene::UpdateAllPrimitiveSceneInfos(FRDGBuilder& GraphBuilder, bool bAsync
 			// free the primitive scene proxy.
 			delete PrimitiveSceneInfo->Proxy;
 			delete PrimitiveSceneInfo;
-
+		}
+		if (DeletedSceneInfos.Num() > 0)
+		{
 			// Invalidate PathTraced image because we removed something from the scene
-			bPathTracingNeedsInvalidation = true;
+			InvalidatePathTracedOutput();
 		}
 	}
 	UpdatedAttachmentRoots.Empty();
