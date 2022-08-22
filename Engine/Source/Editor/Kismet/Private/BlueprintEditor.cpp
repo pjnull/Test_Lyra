e284cffc0a3ed5bae704587711194d5199ce9df2
jaime cifuentes <jaime.cifuentes@epicgames.com>
2022-08-22 03:29:07 -0400

    Fix crash when duplicating a state machine directly in the MyBlueprint panel within an Animation BP, by adding node clone functionality to the BlueprintEditor
    #rb Phillip.Kavan
    #jira UE-161277
    #preflight 63032e92a45b007ea23565da
    
    [CL 21480587 by jaime cifuentes in ue5-release-engine-staging branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
@@ -2247,6 +2247,15 @@ void FBlueprintEditor::RemoveNamespace(const FString& InNamespace)
 	}
 }
 
+void FBlueprintEditor::SelectAndDuplicateNode(UEdGraphNode* InNode)
+{
+	check(InNode != nullptr);
+
+	ClearSelectionStateFor(CurrentUISelection);
+	AddToSelection(InNode);
+	DuplicateNodes();
+}
+
 void FBlueprintEditor::RegisterTabSpawners(const TSharedRef<class FTabManager>& InTabManager)
 {
 	//@TODO: Can't we do this sooner?
