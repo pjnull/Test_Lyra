3377a914ced713decc17ccaf59ba1bc43e738b97
bryan sefcik <bryan.sefcik@epicgames.com>
2022-09-09 20:31:23 -0400

    Pass 3 on cleaning up build.cs files.
    #jira
    #preflight 631b9c15d31788ea3ab0f27b
    
    [CL 21933321 by bryan sefcik in ue5-release-engine-staging branch]

--- Engine/Plugins/Experimental/Chooser/Source/Chooser/Chooser.Build.cs
+++ Engine/Plugins/Experimental/Chooser/Source/Chooser/Chooser.Build.cs
@@ -5,19 +5,7 @@ namespace UnrealBuildTool.Rules
 	public class Chooser : ModuleRules
 	{
 		public Chooser(ReadOnlyTargetRules Target) : base(Target)
-		{
-			PublicIncludePaths.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-				}
-			);
-			
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					// ... add other private include paths required here ...
-				}
-			);
-			
+		{			
 			PublicDependencyModuleNames.AddRange(
 				new string[]
 				{
@@ -33,13 +21,6 @@ namespace UnrealBuildTool.Rules
 					"CoreUObject",
 					"Engine",
 					// ... add private dependencies that you statically link with here ...
-				}
-			);
-
-			DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
 				}
 			);
 		}
