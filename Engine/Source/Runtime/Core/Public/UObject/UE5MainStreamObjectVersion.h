87db09a02e4404001511c1989d33b11b156882ca
marc audy <Marc.Audy@epicgames.com>
2022-09-15 11:49:06 -0400

    [Backout] - CL22025456 [FYI] wei.liu Original CL Desc #lockdown JeanFrancois.Dube ----------------------------------------------------------------- Change the reflection capture's encoded HDR data to RG11B10 format.
    Skip reflectio for simple translucent materials for both mobile forward and deferred.
    
    Translucent materials fall back to single reflection or sky capture on mobile deferred if clustered reflection is disabled.
    
    #jira none
    
    #[robomerge] 5.1
    
    #rb Dmitriy.Dyomin
    #preflight 6322cc7ae93a80888ce672a1
    
    #ushell-cherrypick of 22025079 by Wei.Liu
    
    [CL 22030332 by marc audy in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Core/Public/UObject/UE5MainStreamObjectVersion.h
+++ Engine/Source/Runtime/Core/Public/UObject/UE5MainStreamObjectVersion.h
@@ -280,9 +280,6 @@ struct CORE_API FUE5MainStreamObjectVersion
 		// Replace FNames for class/actor paths with FSoftObjectPath
 		WorldPartitionActorDescActorAndClassPaths,
 
-		// Uses RG11B10 format to store the encoded reflection capture data on mobile
-		StoreReflectionCaptureEncodedHDRDataInRG11B10Format,
-
 		// -----<new versions can be added above this line>-------------------------------------------------
 		VersionPlusOne,
 		LatestVersion = VersionPlusOne - 1
