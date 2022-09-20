373c648bdef52c502b42b8f3f01f089a38cac07c
jeanfrancois dube <JeanFrancois.Dube@epicgames.com>
2022-09-20 17:14:23 -0400

    World Partition: disable invalid actor files detection temporarily until we figure out why it fails during HLOD generation.
    #rb none
    [FYI] sebastien.lussier
    #preflight none
    #rnx
    
    [CL 22102855 by jeanfrancois dube in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/WorldPartition/WorldPartitionActorDescUtils.cpp
+++ Engine/Source/Runtime/Engine/Private/WorldPartition/WorldPartitionActorDescUtils.cpp
@@ -64,7 +64,7 @@ TUniquePtr<FWorldPartitionActorDesc> FWorldPartitionActorDescUtils::GetActorDesc
 			UE_LOG(LogWorldPartition, Warning, TEXT("Invalid class for actor guid `%s` ('%s') from package '%s'"), *NewActorDesc->GetGuid().ToString(), *NewActorDesc->GetActorName().ToString(), *NewActorDesc->GetActorPackage().ToString());
 			return nullptr;
 		}
-		else if (UClass* Class = FindObject<UClass>(InAssetData.AssetClassPath); !Class)
+		/*else if (UClass* Class = FindObject<UClass>(InAssetData.AssetClassPath); !Class)
 		{
 			// We can't detect mising BP classes for inactive plugins, etc.
 			if (InAssetData.AssetClassPath.GetPackageName().ToString().StartsWith(TEXT("/Game/")))
@@ -79,7 +79,7 @@ TUniquePtr<FWorldPartitionActorDesc> FWorldPartitionActorDescUtils::GetActorDesc
 					return nullptr;
 				}
 			}
-		}
+		}*/
 
 		return NewActorDesc;
 	}
