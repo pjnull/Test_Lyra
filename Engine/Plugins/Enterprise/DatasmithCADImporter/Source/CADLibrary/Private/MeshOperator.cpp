515c851976e8fe5f78c966702ee1da3582ba5985
david lesage <David.Lesage@epicgames.com>
2022-09-27 08:12:11 -0400

    #jira UE-157259 : CAD Import in Visual Dataprep Crashes UE Fix the update of material slot names of MeshDescription. The cash was due to a different size of the MeshDescription before and after a conversion into a DynamicMesh Furthermore, the initial process build also a unwanted set of PolygoneGroup du to the call of CADLibrary::CopyPatchGroups
    CADLibrary::CopyPatchGroups was a wrong way to save the patch groups to retesselate with the RetessellateOptions.RetessellationRule "EDatasmithCADRetessellationRule::SkipDeletedSurfaces"
    The retessellation process is improved by saving the the patch groups to retesselate into FMeshConversionContext
    
    This fix also takes care to:
    - not create unused MaterialSlot.
    - not create empty "BodyMesh::Face"
    
    #rb jeanluc.corenthin
    #preflight 6332e4a1a907d7192f26d494
    
    [CL 22203705 by david lesage in ue5-release-engine-staging branch]

--- Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CADLibrary/Private/MeshOperator.cpp
+++ Engine/Plugins/Enterprise/DatasmithCADImporter/Source/CADLibrary/Private/MeshOperator.cpp
@@ -286,8 +286,7 @@ void MeshOperator::ResolveTJunctions(FMeshDescription& MeshDescription, double T
 		BoundaryEdgeCount = MergeCoincidentEdges.FinalNumBoundaryEdges;
 	}
 
-	// TODO: Should not be needed as soon as CopyMaterialSlotNames, and CopyPatchGroups are no more called
-	FMeshDescription NewMeshDescription(MeshDescription);
+	CADLibrary::FMeshDescriptionDataCache MeshDescriptionDataCache(MeshDescription);
 
 	{
 		FConversionToMeshDescriptionOptions ConversionOptions;
@@ -300,13 +299,8 @@ void MeshOperator::ResolveTJunctions(FMeshDescription& MeshDescription, double T
 		ConversionOptions.bTransformVtxColorsSRGBToLinear = false;
 
 		FDynamicMeshToMeshDescription ConverterToMeshDescription(ConversionOptions);
-		ConverterToMeshDescription.Convert(&DynamicMesh, NewMeshDescription);
+		ConverterToMeshDescription.Convert(&DynamicMesh, MeshDescription);
 	}
 
-
-	// TODO: Should not be needed as soon as ConverterToMeshDescription.Convert won't lose data anymore
-	CADLibrary::CopyMaterialSlotNames(MeshDescription, NewMeshDescription);
-	CADLibrary::CopyPatchGroups(MeshDescription, NewMeshDescription);
-
-	MeshDescription = MoveTemp(NewMeshDescription);
+	MeshDescriptionDataCache.RestoreMaterialSlotNames(MeshDescription);
 }
