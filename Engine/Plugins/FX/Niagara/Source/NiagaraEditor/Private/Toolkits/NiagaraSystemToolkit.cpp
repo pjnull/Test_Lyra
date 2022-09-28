cb4914dc5e133b9bde45585a394a2eaa05bca5d3
rob krajcarski <Rob.Krajcarski@epicgames.com>
2022-09-28 10:59:15 -0400

    When saving from the NiagaraSystemToolkit we make sure that we're saving the version not in edit mode.  This prevents recompilation when someone references the system (in a level, or a cook, ...) #jira UE-153263 #rb stu.mckenna #preflight 6333682b1003050806f485f2
    [CL 22222726 by rob krajcarski in ue5-release-engine-staging branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Toolkits/NiagaraSystemToolkit.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraEditor/Private/Toolkits/NiagaraSystemToolkit.cpp
@@ -1207,6 +1207,7 @@ void FNiagaraSystemToolkit::SaveAsset_Execute()
 	}
 	SystemViewModel->NotifyPreSave();
 	FAssetEditorToolkit::SaveAsset_Execute();
+	SystemViewModel->NotifyPostSave();
 }
 
 void FNiagaraSystemToolkit::SaveAssetAs_Execute()
@@ -1218,6 +1219,7 @@ void FNiagaraSystemToolkit::SaveAssetAs_Execute()
 	}
 	SystemViewModel->NotifyPreSave();
 	FAssetEditorToolkit::SaveAssetAs_Execute();
+	SystemViewModel->NotifyPostSave();
 }
 
 bool FNiagaraSystemToolkit::OnRequestClose()
