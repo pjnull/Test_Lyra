f2a39ad713bf0116d1f60720cfc2547e5ac8ced5
eric knapik <Eric.Knapik@epicgames.com>
2022-10-14 20:32:13 -0400

    Chaos : Fix issues with level attribute not being saved properly and that can result in a empty array in non-editor build, potentially causing crashes
    #rb brice.criswell
    #preflight 634086dad718720d2da2fd22
    
    [CL 22544155 by eric knapik in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/GeometryCollection/ManagedArrayCollection.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/GeometryCollection/ManagedArrayCollection.cpp
@@ -118,6 +118,16 @@ bool FManagedArrayCollection::IsAttributeDirty(FName Name, FName Group) const
 	return false;
 }
 
+bool FManagedArrayCollection::IsAttributePersistent(FName Name, FName Group) const
+{
+	const FKeyType Key = FManagedArrayCollection::MakeMapKey(Name, Group);
+	if (const FValueType* Attribute = Map.Find(Key))
+	{
+		return Attribute->Saved;
+	}
+	return false;
+}
+
 TArray<FName> FManagedArrayCollection::AttributeNames(FName Group) const
 {
 	TArray<FName> AttributeNames;
@@ -536,7 +546,6 @@ void FManagedArrayCollection::Serialize(Chaos::FChaosArchive& Ar)
 			Map.Remove(Key);
 		}
 
-#if WITH_EDITOR
 		//it's possible new entries have been added but are not in old content. Resize these.
 		for (TTuple<FKeyType, FValueType>& Pair : Map)
 		{
@@ -546,8 +555,6 @@ void FManagedArrayCollection::Serialize(Chaos::FChaosArchive& Ar)
 				Pair.Value.Value->Resize(GroupSize);
 			}
 		}
-
-#endif
 
 		// strip out GUID Attributes
 		for (auto& GroupName : GroupNames())
