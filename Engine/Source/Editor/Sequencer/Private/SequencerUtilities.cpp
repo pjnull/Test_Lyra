0d0499ecdbf7b833199e00f25d319efcda32f077
max chen <Max.Chen@epicgames.com>
2022-10-24 11:49:35 -0400

    Sequencer: Prevent infinite sections from being pasted on top of each other.
    #jira UE-168014
    #rb matt.hoffman
    #preflight 6353306a0313c2497454d851
    
    [CL 22728801 by max chen in ue5-release-engine-staging branch]

--- Engine/Source/Editor/Sequencer/Private/SequencerUtilities.cpp
+++ Engine/Source/Editor/Sequencer/Private/SequencerUtilities.cpp
@@ -1737,6 +1737,17 @@ bool FSequencerUtilities::PasteSections(const FString& TextToImport, FMovieScene
 
 			Section->ClearFlags(RF_Transient);
 			Section->Rename(nullptr, Track);
+
+			if (Track->SupportsMultipleRows())
+			{
+				Section->SetRowIndex(RowIndex);
+			}
+			else if (!Section->HasStartFrame() && !Section->HasEndFrame())
+			{
+				// If the track doesn't support multiple rows and the pasted section is infinite, it should win out over existing sections
+				Track->RemoveAllAnimationData();
+			}
+
 			Track->AddSection(*Section);
 			if (Section->HasStartFrame())
 			{
@@ -1744,10 +1755,6 @@ bool FSequencerUtilities::PasteSections(const FString& TextToImport, FMovieScene
 				Section->MoveSection(NewStartFrame - Section->GetInclusiveStartFrame());
 			}
 
-			if (Track->SupportsMultipleRows())
-			{
-				Section->SetRowIndex(RowIndex);
-			}
 			OutSections.Add(Section);
 		}
 
