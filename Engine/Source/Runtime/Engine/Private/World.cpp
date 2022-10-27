8822dae041671598a2170c8d7011f5e99ed733c0
pj kack <PJ.Kack@epicgames.com>
2022-10-27 06:18:06 -0400

    Fix race conditions for registration and deregistration of delegates for FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed. Fix crash in TMulticastDelegateBase<FDefaultDelegateUserPolicy>::CompactInvocationList() when called from UWorld::BeginDestroy(). Move deregistration of delegates in FNiagaraDataInterfaceProxy derived classes from destructor on RT to OnDestroyed() on GT. Move registration and deregistration of delegates in UWorld from constructor on ALT and destructor on GT to SetAudioDevice() on GT. Only register the delegate for UWorlds with an audio device set. This reduces the number of registrations from thousands to a handful when using World Partition.
    #rb carlmagnus.nordin, phil.popp
    #rnx
    #preflight 635a45da6a2a692f5d5e589e,635a39edf1493f97864b9ef1
    
    [CL 22806998 by pj kack in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Engine/Private/World.cpp
+++ Engine/Source/Runtime/Engine/Private/World.cpp
@@ -429,15 +429,6 @@ UWorld::UWorld( const FObjectInitializer& ObjectInitializer )
 
 	PerfTrackers = new FWorldInGamePerformanceTrackers();
 
-	AudioDeviceDestroyedHandle = FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.AddLambda([this](const Audio::FDeviceId InDeviceId)
-	{
-		if (InDeviceId == AudioDeviceHandle.GetDeviceID())
-		{
-			FAudioDeviceHandle EmptyHandle;
-			SetAudioDevice(EmptyHandle);
-		}
-	});
-
 	IsInBlockTillLevelStreamingCompleted = 0;
 	BlockTillLevelStreamingCompletedEpoch = 0;
 }
@@ -1051,14 +1042,9 @@ void UWorld::BeginDestroy()
 		Scene->UpdateParameterCollections(TArray<FMaterialParameterCollectionInstanceResource*>());
 	}
 
-	if (AudioDeviceDestroyedHandle.IsValid())
-	{
-		FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.Remove(AudioDeviceDestroyedHandle);
-		AudioDeviceDestroyedHandle.Reset();
-	}
-
 	FAudioDeviceHandle EmptyHandle;
 	SetAudioDevice(EmptyHandle);
+	check(!AudioDeviceDestroyedHandle.IsValid());
 
 #if UE_WITH_IRIS
 	if (IrisSystemHolder.IsHolding())
@@ -8711,6 +8697,8 @@ void UWorld::AddPostProcessingSettings(FVector ViewLocation, FSceneView* SceneVi
 
 void UWorld::SetAudioDevice(const FAudioDeviceHandle& InHandle)
 {
+	check(IsInGameThread());
+
 	if (InHandle.GetDeviceID() == AudioDeviceHandle.GetDeviceID())
 	{
 		return;
@@ -8738,6 +8726,23 @@ void UWorld::SetAudioDevice(const FAudioDeviceHandle& InHandle)
 	else
 	{
 		AudioDeviceHandle.Reset();
+	}
+
+	if (AudioDeviceHandle.IsValid() && !AudioDeviceDestroyedHandle.IsValid())
+	{
+		AudioDeviceDestroyedHandle = FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.AddLambda([this](const Audio::FDeviceId InDeviceId)
+		{
+			if (InDeviceId == AudioDeviceHandle.GetDeviceID())
+			{
+				FAudioDeviceHandle EmptyHandle;
+				SetAudioDevice(EmptyHandle);
+			}
+		});
+	}
+	else if (!AudioDeviceHandle.IsValid() && AudioDeviceDestroyedHandle.IsValid())
+	{
+		FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.Remove(AudioDeviceDestroyedHandle);
+		AudioDeviceDestroyedHandle.Reset();
 	}
 }
 
