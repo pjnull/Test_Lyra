59d9244574458c8e8239dc2678a69aedc2a53752
brice criswell <Brice.Criswell@epicgames.com>
2022-11-04 00:46:51 -0400

    Dataflow : Node Rendering #rb none #preflight 635b193e1b7de936a53457dd
    [CL 22978622 by brice criswell in ue5-release-engine-staging branch]

--- Engine/Source/Runtime/Experimental/Dataflow/Core/Public/Dataflow/DataflowNodeParameters.h
+++ Engine/Source/Runtime/Experimental/Dataflow/Core/Public/Dataflow/DataflowNodeParameters.h
@@ -16,10 +16,26 @@ namespace Dataflow
 {
 	struct DATAFLOWCORE_API FTimestamp
 	{
-		static uint64 Invalid;
-		FTimestamp(uint64 InValue) : Value(InValue) {}
-		uint64 Value = 0;
+		typedef uint64 Type;
+		Type Value = Type(0);
+
+		FTimestamp(Type InValue) : Value(InValue) {}
 		bool operator>=(const FTimestamp& InTimestamp) { return Value >= InTimestamp.Value; }
+		bool IsInvalid() { return Value == Invalid; }
+
+		static Type Current();
+		static Type Invalid; // 0
+	};
+
+	struct DATAFLOWCORE_API FRenderingParameter {
+		FRenderingParameter() {}
+		FRenderingParameter(FName InTypeName, const TArray<FName>& InOutputs)
+			: Type(InTypeName), Outputs(InOutputs) {}
+		FRenderingParameter(FName InTypeName, TArray<FName>&& InOutputs)
+			: Type(InTypeName), Outputs(InOutputs) {}
+
+		FName Type = FName("");
+		TArray<FName> Outputs;
 	};
 
 	struct FContextCacheElementBase 
@@ -105,7 +121,7 @@ namespace Dataflow
 		void SetData(size_t Key, FProperty* Property, const T& Value)
 		{
 			int64 IntKey = (int64)Key;
-			TUniquePtr<FContextCacheElement<T>> DataStoreEntry = MakeUnique<FContextCacheElement<T>>(Property, Value, FPlatformTime::Cycles64());
+			TUniquePtr<FContextCacheElement<T>> DataStoreEntry = MakeUnique<FContextCacheElement<T>>(Property, Value, FTimestamp::Current());
 
 			SetDataImpl(IntKey, MoveTemp(DataStoreEntry));
 		}
@@ -114,7 +130,7 @@ namespace Dataflow
 		void SetData(size_t Key, FProperty* Property, T&& Value)
 		{
 			int64 IntKey = (int64)Key;
-			TUniquePtr<FContextCacheElement<T>> DataStoreEntry = MakeUnique<FContextCacheElement<T>>(Property, Forward<T>(Value), FPlatformTime::Cycles64());
+			TUniquePtr<FContextCacheElement<T>> DataStoreEntry = MakeUnique<FContextCacheElement<T>>(Property, Forward<T>(Value), FTimestamp::Current());
 
 			SetDataImpl(IntKey, MoveTemp(DataStoreEntry));
 		}
@@ -140,7 +156,15 @@ namespace Dataflow
 			int64 IntKey = (int64)Key;
 			return HasDataImpl(Key, InTimestamp);
 		}
-		
+
+		virtual bool IsEmptyImpl() const = 0;
+
+		bool IsEmpty() const
+		{
+			return IsEmptyImpl();
+		}
+
+
 		FTimestamp GetTimestamp() const { return Timestamp; }
 		virtual void Evaluate(const FDataflowNode* Node, const FDataflowOutput* Output) = 0;
 		virtual bool Evaluate(const FDataflowOutput& Connection) = 0;
@@ -169,6 +193,11 @@ namespace Dataflow
 		virtual bool HasDataImpl(int64 Key, FTimestamp InTimestamp = FTimestamp::Invalid) override
 		{
 			return DataStore.Contains(Key) && DataStore[Key]->Timestamp >= InTimestamp;
+		}
+
+		virtual bool IsEmptyImpl() const override
+		{
+			return DataStore.IsEmpty();
 		}
 
 		virtual void Evaluate(const FDataflowNode* Node, const FDataflowOutput* Output) override;
@@ -207,6 +236,11 @@ namespace Dataflow
 			CacheLock->Lock(); ON_SCOPE_EXIT { CacheLock->Unlock(); };
 			
 			return DataStore.Contains(Key) && DataStore[Key]->Timestamp >= InTimestamp;
+		}
+
+		virtual bool IsEmptyImpl() const override
+		{
+			return DataStore.IsEmpty();
 		}
 
 		virtual void Evaluate(const FDataflowNode* Node, const FDataflowOutput* Output) override;
