01af2dc4c6b9b36f4501637cba3ac676d57e5399
Devin Doucette <devin.doucette@epicgames.com>
2023-01-30 12:36:46 -0500

    Logging: Fixed the static asserts about the format string to catch non-const format strings
    #jira UE-175373
    #preflight 63d444cef626715201a7f5b8
    #preflight 63d444e85354589b5c0ff3df
    #rb Zousar.Shaker
    
    [CL 23910762 by Devin Doucette in 5.2 branch]

--- Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
+++ Engine/Source/Runtime/Engine/Private/ShaderCompiler/ShaderCompiler.cpp
@@ -3120,7 +3120,7 @@ FShaderCompilerStats* GShaderCompilerStats = nullptr;
 void FShaderCompilerStats::WriteStats(FOutputDevice* Ar)
 {
 #if ALLOW_DEBUG_FILES
-	static TCHAR DebugText[] = TEXT("Wrote shader compile stats to file '%s'.");
+	constexpr static const TCHAR DebugText[] = TEXT("Wrote shader compile stats to file '%s'.");
 	{
 		FlushRenderingCommands();
 
