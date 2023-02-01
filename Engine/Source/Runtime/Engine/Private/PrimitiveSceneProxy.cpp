2f31927e1ffd63d166acf5a484b0a2a12111337b
Patrick Kelly <Patrick.Kelly@epicgames.com>
2023-02-01 09:10:47 -0500

    Add heterogeneous volumes property to FPrimitiveSceneProxy. - Expose heterogeneous volumes property to Niagara MeshRenderer Properties - Conditionalize use of homogeneous volumes rendering based on existence of this property.
    #rb stu.mckenna
    #jira UE-175742
    #preflight 63da67e765738ba9514a88cc
    
    [CL 23947004 by Patrick Kelly in 5.2 branch]

--- Engine/Source/Runtime/Engine/Private/PrimitiveSceneProxy.cpp
+++ Engine/Source/Runtime/Engine/Private/PrimitiveSceneProxy.cpp
@@ -249,6 +249,7 @@ FPrimitiveSceneProxy::FPrimitiveSceneProxy(const UPrimitiveComponent* InComponen
 ,	bStaticElementsAlwaysUseProxyPrimitiveUniformBuffer(false)
 ,	bVFRequiresPrimitiveUniformBuffer(true)
 ,	bIsNaniteMesh(false)
+,	bIsHeterogeneousVolume(false)
 ,	bIsHierarchicalInstancedStaticMesh(false)
 ,	bIsLandscapeGrass(false)
 ,	bSupportsGPUScene(false)
