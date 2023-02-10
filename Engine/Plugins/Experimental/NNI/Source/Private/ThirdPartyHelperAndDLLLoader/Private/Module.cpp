73c47aa311024dd30e5c63b45c688671e80e6d9b
nebojsa dragosavac <nebojsa.dragosavac@3lateral.com>
2023-02-10 16:51:30 -0500

    Remove warning display
    #preflight none
    #rnx
    #jira BuildBreak
    #rb Merge
    
    [CL 24127940 by nebojsa dragosavac in 5.2 branch]

--- Engine/Plugins/Experimental/NNI/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
+++ Engine/Plugins/Experimental/NNI/Source/Private/ThirdPartyHelperAndDLLLoader/Private/Module.cpp
@@ -11,11 +11,6 @@
 void FThirdPartyHelperAndDLLLoaderModule::StartupModule()
 {
 #ifdef WITH_DIRECTML
-		UE_LOG(
-			LogNeuralNetworkInferenceThirdPartyHelperAndDLLLoader, 
-			Warning, 
-			TEXT("Please disable NNE plugin, both NeuralNetworkInference and NNE plugins can't be used at a same time"));
-		
 	const FString DirectMLRuntimeBinPath = FString(FPlatformProcess::BaseDir()) / TEXT(PREPROCESSOR_TO_STRING(DIRECTML_PATH));
 	const FString DirectMLDLLPath = DirectMLRuntimeBinPath / TEXT("DirectML.dll");
 
