9123b8bf609b224fb32458681b9dab9b6039ed26
Krzysztof Narkowicz <Krzysztof.Narkowicz@epicgames.com>
2023-02-17 17:26:42 -0500

    Lumen: * Fix incorrect reflections when using r.Lumen.Reflections.SampleSceneColorAtHit. Screen space hit points are now also discarded based on the comparison between reconstructed geometric normal and ray hit geometric normal. * Fix surface cache sampling with hit lighting due to using detail normal instead of a geometric normal
    #rb Daniel.Wright, Charles.deRousiers
    #preflight 63efca0961378b7ea0361104
    #jira UE-177485
    #lockdown Juan.Canada
    
    [CL 24292627 by Krzysztof Narkowicz in 5.2 branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenReflectionHardwareRayTracing.cpp
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenReflectionHardwareRayTracing.cpp
@@ -135,6 +135,7 @@ class FLumenReflectionHardwareRayTracing : public FLumenHardwareRayTracingShader
 		SHADER_PARAMETER_RDG_BUFFER_SRV(Buffer<uint2>, TraceTexelDataPacked)
 		SHADER_PARAMETER_STRUCT_INCLUDE(FLumenHZBScreenTraceParameters, HZBScreenTraceParameters)
 		SHADER_PARAMETER(float, RelativeDepthThickness)
+		SHADER_PARAMETER(float, SampleSceneColorNormalTreshold)
 		SHADER_PARAMETER(int32, SampleSceneColor)
 
 		SHADER_PARAMETER(int, ThreadCount)
@@ -384,6 +385,7 @@ void SetLumenHardwareRayTracingReflectionParameters(
 
 	extern float GLumenReflectionSampleSceneColorRelativeDepthThreshold;
 	Parameters->RelativeDepthThickness = GLumenReflectionSampleSceneColorRelativeDepthThreshold;
+	Parameters->SampleSceneColorNormalTreshold = LumenReflections::GetSampleSceneColorNormalTreshold();
 	Parameters->SampleSceneColor = bSampleSceneColorAtHit ? 1 : 0;
 
 	Parameters->ThreadCount = DefaultThreadCount;
