ce35512da13cd6fecd174880a1e074cf08528b28
halfdan ingvarsson <halfdan.ingvarsson@epicgames.com>
2023-02-28 14:14:32 -0500

    Control Rig: Fix a out-of-bounds memory access error in the Harmonics node.
    #jira UE-176381
    #rb kiaran.ritchie
    #preflight 63fd4967ae54ee4ce959958e
    #rnx
    
    [CL 24447604 by halfdan ingvarsson in 5.2 branch]

--- Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Highlevel/Harmonics/RigUnit_BoneHarmonics.cpp
+++ Engine/Plugins/Animation/ControlRig/Source/ControlRig/Private/Units/Highlevel/Harmonics/RigUnit_BoneHarmonics.cpp
@@ -80,18 +80,19 @@ FRigUnit_ItemHarmonics_Execute()
 
 	if (CachedItems.Num() != Targets.Num())
 	{
-		CachedItems.Reset();
+		CachedItems.Reset(Targets.Num());
+		
 		WaveTime = FVector::ZeroVector;
-	}
 
-	for (int32 ItemIndex = 0; ItemIndex < Targets.Num(); ItemIndex++)
-	{
-		FCachedRigElement CachedItem(Targets[ItemIndex].Item, Hierarchy);
-		if (!CachedItem.IsValid())
+		for (int32 ItemIndex = 0; ItemIndex < Targets.Num(); ItemIndex++)
 		{
-			UE_CONTROLRIG_RIGUNIT_REPORT_WARNING(TEXT("Item not found."));
+			FCachedRigElement CachedItem(Targets[ItemIndex].Item, Hierarchy);
+			if (!CachedItem.IsValid())
+			{
+				UE_CONTROLRIG_RIGUNIT_REPORT_WARNING(TEXT("Item not found."));
+			}
+			CachedItems.Add(CachedItem);
 		}
-		CachedItems.Add(CachedItem);
 	}
 
 	for (int32 ItemIndex = 0; ItemIndex < CachedItems.Num(); ItemIndex++)
