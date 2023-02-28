d3fed87f7f09cf8a61e67fc8625e831346c073fe
Josh Adams <josh.adams@epicgames.com>
2023-02-28 08:28:25 -0500

    - Fixed .precompiled file location to be architcture independent - Added IntermediateDirectoryNoArch to UEBuildModule (we can use this in the future to put generated definitions headers, which can help reduce rebuilds going between multi- and single-arch) #rb Joe.kirchoff #jira UE-178211,UE-178589 #preflight 63fd3cb72b1a2765f9a46322
    [CL 24441031 by Josh Adams in 5.2 branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModule.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModule.cs
@@ -30,6 +30,11 @@ namespace UnrealBuildTool
 		public readonly DirectoryReference IntermediateDirectory;
 
 		/// <summary>
+		/// The directory for this module's generated files that are architecture independent
+		/// </summary>
+		public readonly DirectoryReference IntermediateDirectoryNoArch;
+
+		/// <summary>
 		/// The name that uniquely identifies the module.
 		/// </summary>
 		public string Name
@@ -209,11 +214,13 @@ namespace UnrealBuildTool
 		/// </summary>
 		/// <param name="Rules">Rules for this module</param>
 		/// <param name="IntermediateDirectory">Intermediate directory for this module</param>
+		/// <param name="IntermediateDirectoryNoArch">Intermediate directory for this module for files that are architecture independent</param>
 		/// <param name="Logger">Logger for output</param>
-		public UEBuildModule(ModuleRules Rules, DirectoryReference IntermediateDirectory, ILogger Logger)
+		public UEBuildModule(ModuleRules Rules, DirectoryReference IntermediateDirectory, DirectoryReference IntermediateDirectoryNoArch, ILogger Logger)
 		{
 			this.Rules = Rules;
 			this.IntermediateDirectory = IntermediateDirectory;
+			this.IntermediateDirectoryNoArch = IntermediateDirectoryNoArch;
 
 			ModuleApiDefine = Name.ToUpperInvariant() + "_API";
 
