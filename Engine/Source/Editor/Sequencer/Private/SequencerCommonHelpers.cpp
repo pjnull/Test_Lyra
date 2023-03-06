4a69f0ebfde2bd5f37942739464209f0cde89173
Max Chen <Max.Chen@epicgames.com>
2023-03-06 15:51:41 -0500

    Sequencer: Disable some right click menus if the sequence is read only (Add Section, Blending Algorithm, Edit/Batch Edit Sections)
    #jira UE-179236
    #preflight 64063d182559570cc76cd02e
    #lockdown aurel.cordonnier
    #rb matt.hoffman, andrew.rodham
    
    [CL 24529375 by Max Chen in 5.2 branch]

--- Engine/Source/Editor/Sequencer/Private/SequencerCommonHelpers.cpp
+++ Engine/Source/Editor/Sequencer/Private/SequencerCommonHelpers.cpp
@@ -350,5 +350,6 @@ void SequencerHelpers::AddPropertiesMenu(FSequencer& Sequencer, FMenuBuilder& Me
 	DetailsView->SetObjects(Sections);
 
 	DetailsNotifyWrapper->SetDetailsAndSequencer(DetailsView, Sequencer.AsShared());
+	DetailsNotifyWrapper->SetEnabled(!Sequencer.IsReadOnly());
 	MenuBuilder.AddWidget(DetailsNotifyWrapper, FText::GetEmpty(), true);
 }
