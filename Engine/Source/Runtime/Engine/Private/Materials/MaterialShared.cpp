e19dbaf3315c0b93a2c2093a487f8a09ecb1dac8
dan elksnitis <dan.elksnitis@epicgames.com>
2023-03-08 14:04:59 -0500

    [shaders] remove a sanity check that was incorrect; it's possible that if a shader fails to compile the shader map for a material will be set back to null after the compilation fail is processed (so not having a gamethreadshadermap at the point of the FinishCacheShaders call doesn't necessarily mean we didn't have one during BeginCacheShaders). in this case the logic following the existing sanity check will still function correctly; it will just attempt to compile the shader again and report the compile failure for any other instances/materials which share the shadermap.
    #jira UE-177204
    #preflight
    #rb jason.nadro
    
    [CL 24564204 by dan elksnitis in 5.2 branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialShared.cpp
@@ -2626,7 +2626,6 @@ bool FMaterial::CacheShaders(const FMaterialShaderMapId& ShaderMapId, EShaderPla
 	if (!GameThreadShaderMap)
 	{
 		TRefCountPtr<FMaterialShaderMap> ShaderMap;
-		check(!bFoundCompiling); // sanity check
 		if (CacheShadersPending.IsValid()) // we started a load above, check the result
 		{
 			ShaderMap = CacheShadersPending->Get();
