34438ad6bf9ed9797c13fdade6485b5c4ddd198c
Joe Kirchoff <joe.kirchoff@epicgames.com>
2023-03-09 12:37:44 -0500

    UnrealBuildTool: Fix issue generating visual studio solution because of incorrect check if a PCH is in use which was causing modules to use an incorrect SharedPCH when a Private PCH should have been used
    #jira UE-177193
    #rb Josh.Adams
    #lockdown Francis.Hurteau
    #preflight 6409298f827cf9251b4fc513
    
    [CL 24578254 by Joe Kirchoff in 5.2 branch]

--- Engine/Source/Programs/UnrealBuildTool/System/CppCompileEnvironment.cs
+++ Engine/Source/Programs/UnrealBuildTool/System/CppCompileEnvironment.cs
@@ -495,7 +495,7 @@ namespace UnrealBuildTool
 		/// <summary>
 		/// True if a single PRecompiledHeader exists, or at least one PerArchPrecompiledHeaderFile exists
 		/// </summary>
-		public bool bHasPrecompiledHeader => PrecompiledHeaderFile != null || (PerArchPrecompiledHeaderFiles != null && PerArchPrecompiledHeaderFiles.Count > 0);
+		public bool bHasPrecompiledHeader => PrecompiledHeaderAction == PrecompiledHeaderAction.Include;
 
 		/// <summary>
 		/// Whether or not UHT is being built
