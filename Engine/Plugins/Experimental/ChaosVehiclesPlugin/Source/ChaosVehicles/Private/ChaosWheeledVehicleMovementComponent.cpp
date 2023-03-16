472ca9d20cca6ef3cf54dfb9dcfe421c0cff6abf
Bill Henderson <Bill.Henderson@epicgames.com>
2023-03-16 16:12:50 -0400

    TP_VehicleAdvBP, fix the offroad vehicle suspension where vehicle wheels are sitting through the ground. The default case for the wheel trace type was the wrong way round (different default in some parts of code than before).
    Recent'ish changes for Opal introduced this, but doesn't affect Opal.
    
    #jira UE-180135
    #rb Michael.Lentine
    #preflight 6412f44a0e2fb991c13e5951
    #lockdown Nick.Whiting
    
    [CL 24678189 by Bill Henderson in 5.2 branch]

--- Engine/Plugins/Experimental/ChaosVehiclesPlugin/Source/ChaosVehicles/Private/ChaosWheeledVehicleMovementComponent.cpp
+++ Engine/Plugins/Experimental/ChaosVehiclesPlugin/Source/ChaosVehicles/Private/ChaosWheeledVehicleMovementComponent.cpp
@@ -603,13 +603,13 @@ void UChaosWheeledVehicleSimulation::ApplySuspensionForces(float DeltaTime, TArr
 						if (FSuspensionConstraintPhysicsProxy* Proxy = Constraint->GetProxy<FSuspensionConstraintPhysicsProxy>())
 						{
 							FVec3 TargetPos;
-							if (WheelTraceParams[WheelIdx].SweepShape == ESweepShape::Raycast)
+							if (WheelTraceParams[WheelIdx].SweepShape == ESweepShape::Spherecast)
 							{
-								TargetPos = HitResult.ImpactPoint + (PWheel.GetEffectiveRadius() * VehicleState.VehicleUpAxis);
+								TargetPos = HitResult.Location;
 							}
 							else
 							{
-								TargetPos = HitResult.Location;
+								TargetPos = HitResult.ImpactPoint + (PWheel.GetEffectiveRadius() * VehicleState.VehicleUpAxis);
 							}
 
 							Chaos::FPhysicsSolver* Solver = Proxy->GetSolver<Chaos::FPhysicsSolver>();
@@ -1216,7 +1216,6 @@ void UChaosWheeledVehicleMovementComponent::FixupSkeletalMesh()
 				}
 			}
 		}
-
 		VehicleSimulationPT->UpdateConstraintHandles(ConstraintHandles); // TODO: think of a better way to communicate this data
 
 		Mesh->KinematicBonesUpdateType = EKinematicBonesUpdateToPhysics::SkipSimulatingBones;
@@ -2866,14 +2865,14 @@ float UChaosWheeledVehicleMovementComponent::GetSuspensionOffset(int WheelIndex)
 					if (HitResult.bBlockingHit)
 					{
 						FVector LocalPos = GetWheelRestingPosition(WheelSetup);
-						if (Wheel->SweepShape == ESweepShape::Raycast)
+						if (Wheel->SweepShape == ESweepShape::Spherecast)
 						{
-							FVector LocalHitPoint = VehicleWorldTransform.InverseTransformPosition(HitResult.ImpactPoint);
+							FVector LocalHitPoint = VehicleWorldTransform.InverseTransformPosition(HitResult.Location);
 							Offset = CalcOffset(Wheel->SweepShape, LocalHitPoint, LocalPos, Wheel->WheelRadius);
 						}
 						else
 						{
-							FVector LocalHitPoint = VehicleWorldTransform.InverseTransformPosition(HitResult.Location);
+							FVector LocalHitPoint = VehicleWorldTransform.InverseTransformPosition(HitResult.ImpactPoint);
 							Offset = CalcOffset(Wheel->SweepShape, LocalHitPoint, LocalPos, Wheel->WheelRadius);
 						}
 
