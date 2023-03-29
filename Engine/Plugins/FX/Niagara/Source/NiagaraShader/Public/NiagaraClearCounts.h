2187c5964a4e5e90accf39762c63c322209bc4b1
Stu McKenna <Stu.McKenna@epicgames.com>
2023-03-29 14:53:06 -0400

    - Add uint variant for clear counts and use with array clear - Fixes a validation issue due to buffer being a uint vs int
    #rb rob.krajcarski
    #jira UE-181610
    #rnx
    #preflight 642475f2051bec41cd295598
    
    [CL 24842294 by Stu McKenna in 5.2 branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraClearCounts.h
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Public/NiagaraClearCounts.h
@@ -9,7 +9,9 @@
 namespace NiagaraClearCounts
 {
 	NIAGARASHADER_API void ClearCountsInt(FRDGBuilder& GraphBuilder, FRDGBufferUAVRef UAV, TConstArrayView<TPair<uint32, int32>> IndexAndValueArray);
+	NIAGARASHADER_API void ClearCountsUInt(FRDGBuilder& GraphBuilder, FRDGBufferUAVRef UAV, TConstArrayView<TPair<uint32, uint32>> IndexAndValueArray);
 
 	//-TODO:RDG: Deprecated
 	NIAGARASHADER_API void ClearCountsInt(FRHICommandList& RHICmdList, FRHIUnorderedAccessView* UAV, TConstArrayView<TPair<uint32, int32>> IndexAndValueArray);
+	NIAGARASHADER_API void ClearCountsUInt(FRHICommandList& RHICmdList, FRHIUnorderedAccessView* UAV, TConstArrayView<TPair<uint32, uint32>> IndexAndValueArray);
 }
