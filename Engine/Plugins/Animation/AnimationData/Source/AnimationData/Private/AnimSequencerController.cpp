c375ad8761dbbda07a2708d4365366cb1ec13ba6
Jurre deBaare <jurre.debaare@epicgames.com>
2023-04-11 09:43:50 -0400

    //UE5/Release-5.2 - Static Analysis Win64 (PVS-Studio) - Issue 285432 - warning V595: The 'Skeleton' pointer was utilized before it was verified against nullptr #fix nullptr check #jira UE-182699 #preflight 64353f0685398c090c8db504
    [CL 24990379 by Jurre deBaare in 5.2 branch]

--- Engine/Plugins/Animation/AnimationData/Source/AnimationData/Private/AnimSequencerController.cpp
+++ Engine/Plugins/Animation/AnimationData/Source/AnimationData/Private/AnimSequencerController.cpp
@@ -2385,7 +2385,8 @@ void UAnimSequencerController::PopulateWithExistingModel(TScriptInterface<IAnima
 
 		if (const UAnimSequence* AnimSequence = Model->GetAnimationSequence())
 		{
-			const USkeleton* Skeleton = AnimSequence->GetSkeleton();		
+			const USkeleton* Skeleton = AnimSequence->GetSkeleton();
+			if (Skeleton)
 			{
 				QUICK_SCOPE_CYCLE_COUNTER(STAT_PopulateBoneTrack);
 				for (const FBoneAnimationTrack& BoneTrack : BoneTracks)
