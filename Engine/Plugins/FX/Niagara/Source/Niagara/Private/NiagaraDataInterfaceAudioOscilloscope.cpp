c9685f3d1564947ec82735e9ecca5a44a7b0cbc1
jimmy smith <Jimmy.Smith@epicgames.com>
2023-06-05 20:00:59 -0400

    NiagraAudioDataInterface fixes #jira UE-180472 #jira UE-152571 #jira UE-181339 #rbx #lockdown julien.marchand
    [CL 25807496 by jimmy smith in 5.2 branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceAudioOscilloscope.cpp
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Private/NiagaraDataInterfaceAudioOscilloscope.cpp
@@ -37,6 +37,7 @@ FNiagaraDataInterfaceProxyOscilloscope::FNiagaraDataInterfaceProxyOscilloscope(i
 	, ScopeInMilliseconds(InScopeInMillseconds)
 	, NumChannelsInDownsampledBuffer(0)
 {
+	check(IsInGameThread());
 	DeviceCreatedHandle = FAudioDeviceManagerDelegates::OnAudioDeviceCreated.AddRaw(this, &FNiagaraDataInterfaceProxyOscilloscope::OnNewDeviceCreated);
 	DeviceDestroyedHandle = FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.AddRaw(this, &FNiagaraDataInterfaceProxyOscilloscope::OnDeviceDestroyed);
 
@@ -410,6 +411,8 @@ void FNiagaraDataInterfaceProxyOscilloscope::OnBeginDestroy()
 	check(IsInGameThread());
 	FAudioDeviceManagerDelegates::OnAudioDeviceCreated.Remove(DeviceCreatedHandle);
 	FAudioDeviceManagerDelegates::OnAudioDeviceDestroyed.Remove(DeviceDestroyedHandle);
+	DeviceCreatedHandle.Reset();
+	DeviceDestroyedHandle.Reset();
 
 	if (bIsSubmixListenerRegistered)
 	{
